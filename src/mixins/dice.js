8b1ac915baad2a6a05d77d93847473d628871139
Harm Manders <harmmanders@gmail.com>
2018-11-10 21:37:42 +0100

    dice roller has nice console.log

--- src/mixins/dice.js
+++ src/mixins/dice.js
@@ -1,32 +1,38 @@
 export const dice = {
 	created() {
-	// console.log("CREATED MIXIN")
-},
-methods: {
-	rollD(d=20,n=1,m=0) {
-		const add = (a, b) => a + b
-		let throws = []
-		for (var i=0; i < n; i++) {
-			throws.push(Math.ceil(Math.random() * d))
-		}
-		console.log("Rolled: " + throws)
-		
-		return throws.reduce(add) + m * n
+		// console.log("CREATED MIXIN")
 	},
-	rollD6(n=1,m=0) {
-		return this.rollD(6,n,m)
-	},
-	rollD8(n=1,m=0) {
-		return this.rollD(8,n,m)
-	},
-	rollD10(n=1,m=0) {
-		return this.rollD(10,n,m)
-	},
-	rollD20(n=1,m=0) {
-		return this.rollD(20,n,m)
-	},
-	rollD100(n=1,m=0) {
-		return this.rollD(100,n,m)
-	},
-}
+	methods: {
+		rollD(d=20,n=1,m=0) {
+			n=3
+			const add = (a, b) => a + b
+			let throws = []
+			for (var i=0; i < n; i++) {
+				throws.push(Math.ceil(Math.random() * d))
+			}
+
+			let s = ''
+			if (Math.sign(m) > 0) {
+				s = '+'
+			}
+			let sum = throws.reduce(add) + m * n
+			console.log(`Rolled ${n}d${d}${s}${m}: [${throws}] ${s}${m} -> ${sum}`)
+			return sum
+		},
+		rollD6(n=1,m=0) {
+			return this.rollD(6,n,m)
+		},
+		rollD8(n=1,m=0) {
+			return this.rollD(8,n,m)
+		},
+		rollD10(n=1,m=0) {
+			return this.rollD(10,n,m)
+		},
+		rollD20(n=1,m=0) {
+			return this.rollD(20,n,m)
+		},
+		rollD100(n=1,m=0) {
+			return this.rollD(100,n,m)
+		},
+	}
 }

530e985304600fb3790c93e57537b0fcc8833642
Key <keyroos@gmail.com>
2018-11-13 19:07:16 +0100

    Manual damage perfected

--- src/components/combat/Actions.vue
+++ src/components/combat/Actions.vue
@@ -22,61 +22,15 @@
 							<label class="custom-control-label" for="lethal">Lethal damage</label>
 						</div> -->
 						<div class="manual">
-							<input type="phone" v-model="manualAmount" class="form-control">
+							<input type="phone" v-model="manualAmount" v-validate="'required|numeric'" name="Manual Input" class="form-control">
 							<button class="btn dmg bg-red" @click="setManual(target, 'damage')"><i class="fas fa-minus-square"></i></button>
 							<button class="btn heal bg-green" @click="setManual(target, 'healing')"><i class="fas fa-plus-square"></i></button>
 						</div>
+						<p class="validate red" v-if="errors.has('Manual Input')">{{ errors.first('Manual Input') }}</p>
 					</template>
 				</div>
 				<div class="tab-pane fade" id="select" role="tabpanel" aria-labelledby="select-tab">
-					<div class="row">
-						<div class="col">
-							<select class="custom-select mb-3" id="action">
-								<option selected>Choose...</option>
-								<optgroup label="Attacks">
-									<option value="1">Shortbow</option>
-									<option value="2">Scimtar</option>
-								</optgroup>
-							</select>
-						</div>
-						<div class="col">
-							<div id="selected-action">
-								<p>
-									<b>Range:</b> <span class="range"></span><br/>
-									<b>To hit:</b> <span class="to-hit"></span><br/>
-									<b>Damage:</b> <span class="damage"></span><br/>
-									<b>Type:</b> <span class="type"></span>
-								</p>
-							</div>
-						</div>
-					</div>
-					
-					<a class="btn btn-block disabled mb-3"><i class="fas fa-dice"></i> Roll to hit</a>
-					
-					<div id="hit-result">
-						<span>
-							<div class="row my-3">
-								<div class="col-7">
-									<span class="roll">Roll</span>
-									<span class="total">-</span>
-								</div>
-								<div class="col-2 ac">AC</div>
-								<div class="col-1 hit"></div>
-							</div>
-						</span>
-						<a class="btn btn-block disabled"><i class="fas fa-dice"></i> Roll for damage</a>
-					</div>
-					<div id="damage-result">
-						<span>
-							<div class="row my-3">
-								<div class="col">
-									<span class="roll">Roll</span>
-									<span class="total">-</span>
-								</div>
-							</div>
-						</span>
-						<a class="btn btn-block disabled bg-red">Apply damage</a>
-					</div>
+					Coming soon
 				</div>
 			</div>
 		</div>
@@ -101,41 +55,62 @@
 		},
 		methods: {
 			setManual(target, type) {
-				// console.log('MaxHP: ' + target.maxHp);
-				// console.log('Type: ' + type);
-				// console.log('Amount: ' + this.manualAmount);
+				this.$validator.validateAll().then((result) => {
+					if(result) {
+						let amount = parseInt(this.manualAmount);
+						let maxHp = parseInt(target.maxHp);
+						let curHp = parseInt(target.curHp);
 
-				let amount = parseInt(this.manualAmount);
-				let maxHp = parseInt(target.maxHp);
-				let curHp = parseInt(target.curHp);
+						if(type == 'damage') {
+						let newhp = parseInt(curHp - amount);
 
-				if(type == 'damage') {
-					let newhp = parseInt(curHp - amount);
+						if(newhp < 0) { 
+							newhp = 0;
+							//let over = amount - curHp; //overkill
+							amount = curHp;
+						}
+							
+							target.curHp = newhp
+							db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${target.key}`).update({
+								curhp: newhp,
+							})
+							//Notification
+							this.$snotify.error(
+								amount + ' ' + type + 'done to ' + target.name, 
+								'Damage done!', 
+								{
+									position: "centerTop"
+								}
+							);
+						}
+						else {
+							let newhp = parseInt(curHp + amount);
 
-	        if(newhp < 0) { 
-						newhp = 0;
-						let over = amount - curHp; //overkill
-						amount = curHp;
-	        }
-					
-					db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${target.key}`).update({
-						curhp: newhp,
-					})
-				}
-				else {
-					let newhp = parseInt(curHp + amount);
-				
-	        if(newhp > maxHp) { 
-						newhp = maxHp;
-						let over = amount - maxHp + curHp; //overhealing
-						amount = maxHp - curHp;
-	        }
-					
-					db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${target.key}`).update({
-						curhp: newhp,
-					})
-				}
-				this.manualAmount = '';
+							if(newhp > maxHp) { 
+								newhp = maxHp;
+								//let over = amount - maxHp + curHp; //overhealing
+								amount = maxHp - curHp;
+							}
+							
+							target.curHp = newhp
+							db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${target.key}`).update({
+								curhp: newhp,
+							})
+							//Notification
+							this.$snotify.success(
+								amount + ' ' + type + 'done to ' + target.name, 
+								'Healing done!', 
+								{
+									position: "centerTop"
+								}
+							);
+						}
+						this.manualAmount = '';
+					}
+					else {
+						//console.log('Not Valid');
+					}
+				})
 			}
 		}
 	}

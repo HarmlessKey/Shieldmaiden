484de675d57270e12ae9117af9fcfe27bd35b3bb
Key <keyroos@gmail.com>
2019-01-27 15:54:30 +0100

    NPC abilities can be rolled and then applied

--- src/components/combat/actions/Actions.vue
+++ src/components/combat/actions/Actions.vue
@@ -14,91 +14,21 @@
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" 
-					id="select-tab" 
+					id="roll-tab" 
 					data-toggle="tab" 
-					href="#select" 
+					href="#roll" 
 					role="tab" 
-					aria-controls="select" 
+					aria-controls="roll" 
 					aria-selected="false">
-					Select
+					Roll
 				</a>
 			</li>
 		</ul>
 		<div class="scroll" v-bar>
 			<div>
 				<div class="tab-content">
-					<div class="tab-pane fade show active" id="manual" role="tabpanel" aria-labelledby="manual-tab">
-						<p v-if="!target" class="red">No target selected</p>
-						<template v-else>
-							<p>Target: <b class="blue">{{ target.name }}</b></p>
-							<b-form-checkbox class="mb-2" name="crit" v-model="crit">Critical hit</b-form-checkbox>
-
-							<!-- <select class="form-control mb-2" v-model="damageType" name="damageType">
-								<option value="">Type of damage...</option>
-								<option value="acid">Acid</option>
-								<option value="bludgeoning">Bludgeoning</option>
-								<option value="cold">Cold</option>
-								<option value="fire">Fire</option>
-								<option value="force">Force</option>
-								<option value="lightning">Lightning</option>
-								<option value="necrotic">Necrotic</option>
-								<option value="piercing">Piercing</option>
-								<option value="poison">Poison</option>
-								<option value="psychic">Psychic</option>
-								<option value="radiant">Radiant</option>
-								<option value="slashing">Slashing</option>
-								<option value="thunder">Thunder</option>
-							</select> -->
-
-							<div class="manual">
-								<input type="number" 
-									v-model="manualAmount" 
-									v-validate="'numeric'" 
-									name="Manual Input" 
-									min="0"
-									class="form-control manual-input">
-								<button class="btn dmg bg-red" 
-									:class="{disabled: errors.has('Manual Input') || manualAmount == ''}" 
-									@click="setManual(target, 'damage')">
-									<i class="fas fa-minus-square"></i>
-								</button>
-								<button class="btn heal bg-green" 
-									:class="{disabled: errors.has('Manual Input') || manualAmount == ''}" 
-									@click="setManual(target, 'healing')">
-									<i class="fas fa-plus-square"></i>
-								</button>
-							</div>
-							<p class="validate red" v-if="errors.has('Manual Input')">{{ errors.first('Manual Input') }}</p>
-						</template>
-					</div>
-					<div v-if="current" class="tab-pane select fade" id="select" role="tabpanel" aria-labelledby="select-tab">
-						<p v-if="!target" class="red">No target selected</p>
-						<template v-else-if="current.entityType == 'npc'">
-							<p>Target: <b class="blue">{{ target.name }}</b></p>
-							
-							<template v-if="current.actions">
-								<h2>Actions</h2>
-								<div v-for="action, index in current.actions">
-									<a class="d-flex justify-content-between" data-toggle="collapse" :href="'#action-'+index" role="button" aria-expanded="false">
-										<span>{{ index + 1 }}. {{ action.name }}</span>
-										<i class="fas fa-caret-down"></i>
-									</a>
-									<p class="collapse" :id="'action-'+index">{{ action.desc }}</p>
-								</div>
-							</template>
-							<template v-if="current.legendary_actions">
-								<h2>Legendary Actions</h2>
-								<div v-for="action, index in current.legendary_actions">
-									<a class="d-flex justify-content-between" data-toggle="collapse" :href="'#action-'+index" role="button" aria-expanded="false">
-										<span>{{ index + 1 }}. {{ action.name }}</span>
-										<i class="fas fa-caret-down"></i>
-									</a>
-									<p class="collapse" :id="'action-'+index">{{ action.desc }}</p>
-								</div>
-							</template>
-						</template>
-						<p v-else>Players want to roll their own attacks, don't take that away from them.</p>
-					</div>
+					<Manual :current="current" />
+					<Roll :current="current" />	
 				</div>
 			</div>
 		</div>
@@ -109,54 +39,22 @@
 	import firebase from 'firebase'
 	import { db } from '@/firebase'
 	import { mapGetters, mapActions } from 'vuex'
-
 	import { setHP } from '@/mixins/HpManipulations.js'
 
-	export default {
+	import Manual from '@/components/combat/actions/Manual.vue'
+	import Roll from '@/components/combat/actions/Roll.vue'
 
+	export default {
 		name: 'Actions',
+		components: {
+			Manual,
+			Roll,
+		},
 		mixins: [setHP],
 		props: ['current'],
-		data: function() {
-			return {
-				userId: this.$store.getters.getUser.uid,
-				campaignId: this.$route.params.campid,
-				encounterId: this.$route.params.encid,
-				manualAmount: '',
-				damageType: '',
-				crit: false,
-				log: undefined
-			}
-		},
 		computed: {
-			...mapGetters([
-				'encounter',
-				'entities',
-				'turn',
-				'targeted',
-			]),
-			target: function() {
-				return this.entities[this.targeted]
-			}
 		},
 		methods: {
-			setManual(target, type) {
-				this.$validator.validateAll().then((result) => {
-					if(result && this.manualAmount != '') {
-
-						//Update HP
-						this.setHP(this.manualAmount, this.crit, target, this.current, type)
-
-						//Reset input fields
-						this.manualAmount = '';
-						this.damageType = '';
-						this.crit = false;
-					}
-					else {
-						//console.log('Not Valid');
-					}
-				})
-			},
 		},
 	}
 </script>
@@ -186,35 +84,8 @@
 	padding:0 10px 10px 15px;
 	height: calc(100% - 55px);
 }
-.manual {
-	display:grid;
-	grid-template-columns: 2fr 1fr;
-	grid-template-rows: 40px 40px;
-	grid-gap: 10px;
-	grid-template-areas: 
-	"input btn-dmg"
-	"input btn-heal";
-
-	.manual-input {
-		height:90px;
-		font-size:50px;
-		text-align: center;
-		grid-area: input;
-	}
-	.heal {
-		grid-area: btn-heal;
-	}
-	.dmg {
-		grid-area: btn-dmg;
-	}
-}
-
 .tab-content {
 	padding: 0 10px 15px 10px;
 }
-.select {
-	h2 {
-		margin-bottom: 5px !important;
-	}
-}
+
 </style>
---
+++ src/components/combat/actions/Roll.vue
@@ -0,0 +1,143 @@
+<template>
+	<div v-if="current" class="tab-pane roll fade" id="roll" role="tabpanel" aria-labelledby="roll-tab">
+		<p v-if="!target" class="red">No target selected</p>
+		<template v-else-if="current.entityType == 'npc'">
+			<p>Target: <b class="blue">{{ target.name }}</b></p>
+			
+			<template v-if="current.actions">
+				<h2>Actions</h2>
+				<div v-for="action, index in current.actions">
+					<a class="d-flex justify-content-between" 
+						data-toggle="collapse" :href="'#act-'+index" 
+						role="button" 
+						aria-expanded="false"
+						v-if="action['damage_dice']">
+						<span>{{ action.name }}</span>
+						<span class="d-flex justify-content-end">
+							<a @click="roll(action)" class="mr-2"><i class="fas fa-dice-d20"></i></a>
+							<i class="fas fa-caret-down"></i>
+						</span>
+					</a>
+					<p class="collapse" :id="'act-'+index">{{ action.desc }}</p>
+				</div>
+			</template>
+			<template v-if="current.legendary_actions">
+				<h2>Legendary Actions</h2>
+				<div v-for="action, index in current.legendary_actions">
+					<a class="d-flex justify-content-between" 
+						data-toggle="collapse" 
+						:href="'#leg-'+index" role="button" 
+						aria-expanded="false">
+						<span>{{ index + 1 }}. {{ action.name }}</span>
+						<i class="fas fa-caret-down"></i>
+					</a>
+					<p class="collapse" :id="'leg-'+index">{{ action.desc }}</p>
+				</div>
+			</template>
+		</template>
+		<p v-else>Most players want to roll their own attacks, you probably shouldn't take that away from them.</p>
+	</div>
+</template>
+
+<script>
+	import firebase from 'firebase'
+	import { db } from '@/firebase'
+	import { mapGetters, mapActions } from 'vuex'
+	import { dice } from '@/mixins/dice.js'
+	import { setHP } from '@/mixins/HpManipulations.js'
+
+
+	export default {
+		name: 'Select',
+		mixins: [setHP, dice],
+		props: ['current'],
+		data: function() {
+			return {
+				userId: this.$store.getters.getUser.uid,
+				campaignId: this.$route.params.campid,
+				encounterId: this.$route.params.encid,
+			}
+		},
+		computed: {
+			...mapGetters([
+				'encounter',
+				'entities',
+				'turn',
+				'targeted',
+			]),
+			target: function() {
+				return this.entities[this.targeted]
+			}
+		},
+		methods: {
+			roll(action) {
+				event.stopPropagation();
+				var rolls = action['damage_dice'].split('+');
+				let crit = false;
+				let hits = '';
+				var total = 0;
+
+				let toHit =  this.rollD(20, 1, action['attack_bonus'])
+				
+				//Damage
+				for(let roll in rolls) {
+					let dice = rolls[roll].split('d');
+					let damage = this.rollD(dice[1], dice[0], action['damage_bonus']).total;
+
+					total = parseInt(total) + parseInt(damage);
+				}
+				
+				if(toHit.throws[0] == '20') {
+					toHit.total = '<span class="green">NATURAL 20</span>';
+					crit = true;
+				}
+				else if(toHit.throws[0] == '1') {
+					toHit.total = '<span class="red">NATURAL 1</span>';
+				}
+				else {
+					if(toHit.total >= this.target.ac) {
+						hits = `<span class="green">HIT!</span> <span class="gray-hover">(<i class="fas fa-shield"></i> ${this.target.ac})</span>`;
+					}
+					else {
+						hits = `<span class="red">MISS!</span> <span class="gray-hover">(<i class="fas fa-shield"></i> ${this.target.ac})</span>`;
+					}
+				}
+
+				this.$snotify.html(
+					`<div class="snotifyToast__title">
+						<b>${action.name}</b>
+					</div>
+					<div class="snotifyToast__body">
+						<h2>${toHit.total} ${hits}</h2>
+						<h2 class="red">${total} <span class="gray-hover">damage</span></h2>
+					</div> `, {
+					timeout: 0,
+					closeOnClick: false,
+					buttons: [
+						{ 
+							text: 'Apply', 
+							action: (toast) => { 
+								this.setHP(total, crit, this.target, this.current, 'damage')
+								this.$snotify.remove(toast.id); }, 
+								bold: false
+							},
+						{ 
+							text: 'Cancel', 
+							action: (toast) => { 
+								this.$snotify.remove(toast.id); }, 
+								bold: true
+							},
+					]
+				});
+			},
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.select {
+		h2 {
+			margin-bottom: 5px !important;
+		}
+	}
+</style>

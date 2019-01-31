76a939c188f25083dbf151e84e77440a1ab36a61
Harm Manders <harmmanders@gmail.com>
2019-01-31 02:00:34 +0100

    DEPLOY AUTO COMMIT

--- deploy.sh
+++ deploy.sh
@@ -11,29 +11,29 @@ REMOTE_PATH="/var/www/harmlesskey.com/public_html"
 LOCAL_PATH="dist/*"
 BRANCH="master"
 
-printf "\n${GREEN}> STARTING DEPLOYMENT\n"
-printf "${NC}> Checking out master\n\n"
+printf "\n${GREEN}> STARTING DEPLOYMENT${NC}\n"
+printf "> Checking out master\n\n"
 git checkout ${BRANCH}
 if [ $? -ne 0 ]; then
-	printf "\n${RED}>> git checkout ${BRANCH} failed\n"
+	printf "\n${RED}>> git checkout ${BRANCH} failed${NC}\n"
 	exit 1
 fi
-printf "${NC}> Building for production\n"
+printf "> Building for production\n"
 npm run build
 if [ $? -ne 0 ]; then
-	printf "\n${RED}>> npm run build failed\n"
+	printf "\n${RED}>> npm run build failed${NC}\n"
 	exit 1
 fi
 
-printf "${NC}> Committing to git\n"
+printf "> Committing to git\n"
 git commit -am "DEPLOY AUTO COMMIT"
-printf "${NC}> Pushing to ${BRANCH}\n"
+printf "> Pushing to ${BRANCH}\n"
 git push
 
-printf "\n${YELLOW}> DEPLOYING TO ${REMOTE_IP}"
+printf "\n${YELLOW}> DEPLOYING TO ${REMOTE_IP}${NC}\n"
 scp -r ${LOCAL_PATH} ${USER}@${REMOTE_IP}:${REMOTE_PATH}
 if [ $? -ne 0 ]; then
-	printf "\n${RED}>> DEPLOY TO SERVER FAILED!\n"
+	printf "\n${RED}>> DEPLOY TO SERVER FAILED!${NC}\n"
 	exit 1
 fi
-printf "\n${GREEN}> DEPLOY TO SERVER"
+printf "\n${GREEN}> DEPLOY TO SERVER${NC}\n"

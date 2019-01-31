62b22bbf0ae84d2ec89f560524eabc25339be715
Harm Manders <harmmanders@gmail.com>
2019-01-31 01:55:39 +0100

    deploy.sh script now deploys and checksout to master

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,25 @@ REMOTE_IP="37.97.150.110"
 REMOTE_PATH="/var/www/harmlesskey.com/public_html"
 BRANCH="master"
 
-printf "\n${GREEN}Starting deployment\n"
-printf "${NC}Checking out master\n"
-git checkout master
+printf "\n${GREEN}> STARTING DEPLOYMENT\n"
+printf "${NC}> Checking out master\n\n"
+git checkout ${BRANCH}
 if [ $? -ne 0 ]; then
-	printf "\n${RED}git checkout master failed\n"
+	printf "\n${RED}>> git checkout ${BRANCH} failed\n"
 	exit 1
 fi
+printf "${NC}> Building for production\n"
 npm run build
 if [ $? -ne 0 ]; then
-	printf "\n${RED}npm run build failed\n"
+	printf "\n${RED}>> npm run build failed\n"
 	exit 1
 fi
 
+printf "${NC}> Committing to git\n"
 git commit -am "DEPLOY AUTO COMMIT"
+printf "${NC}> Pushing to ${BRANCH}\n"
 git push
 
-printf "\n${GREEN}DEPLOYED TO SERVER\n"
-# scp -r dist/* harm@37.97.150.110:/var/www/harmlesskey.com/public_html
+printf "\n${NC}> DEPLOYING TO ${REMOTE_IP}"
+scp -r dist/* harm@${REMOTE_IP}:${REMOTE_PATH}
+printf "\n${GREEN}> DEPLOY TO SERVER"

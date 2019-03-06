3177f7632b8a4bd407e0491d6cbb96c62e9cb929
Key <keyroos@gmail.com>
2019-03-06 15:01:03 +0100

    Updated sitemap

--- src/views/Sitemap.vue
+++ src/views/Sitemap.vue
@@ -3,12 +3,21 @@
 		<h1>Sitemap</h1>
 		<ul>
 			<li><router-link to="/">Home</router-link></li>
+			<li><router-link to="/sign-up">Sign Up</router-link></li>
 			<li><router-link to="/sign-in">Sign In</router-link></li>
 			<li><router-link to="/documentation">Documentation</router-link></li>
 			<li><router-link to="/feedback">Feedback</router-link></li>
 			<li><router-link to="/updates">Updates</router-link></li>
 			<li><router-link to="/planned">Planned</router-link></li>
 			<li><router-link to="/privacy-policy">Privacy Policy</router-link></li>
+			<li>
+				<router-link to="/compendium">Compendium</router-link>
+				<ul>
+					<li><router-link to="/compendium/conditions">Conditions</router-link></li>
+					<li><router-link to="/compendium/monsters">Monsters</router-link></li>
+					<li><router-link to="/compendium/spells">Spells</router-link></li>
+				</ul>
+			</li>
 		</ul>
 	</div>
 </template>

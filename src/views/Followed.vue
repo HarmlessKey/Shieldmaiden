f086c397e76d46ba8a24050d12c9f7c2354977dc
Key <keyroos@gmail.com>
2019-05-27 19:04:11 +0200

    Improved following view

--- src/views/Followed.vue
+++ src/views/Followed.vue
@@ -2,52 +2,61 @@
 <div>
 	<div class="container">
 		<h2>Followed Users</h2>
-		<template v-if="userInfo && users">
-			<ul v-if="userInfo.follow" class="entities">
-				<li v-for="(following, key) in userInfo.follow" :key="key" class="d-flex justify-content-between">
-					<router-link :to="'/user/' + key" v-if="users[key]">
-						{{ users[key].username }}
+		<template v-if="userInfo">
+			<ul v-if="following" class="entities">
+				<li v-for="(followed, index) in following" :key="index" class="d-flex justify-content-between">
+					<router-link :to="'/user/' + followed.key">
+						{{ followed.username }}
 					</router-link>
-					<template v-if="broadcasting[key]">
-						<span class="live" :class="{ 'active': broadcasting[key].live }">Live</span>
+					<template v-if="followed.live">
+						<span class="live" :class="{ 'active': followed.live }">Live</span>
 					</template>
 				</li>
 			</ul>
 			<p v-else>You are currently not following other users.</p>
 		</template>
+		<div v-else class="loader"><span>Loading Followed Users...</span></div>
 	</div>
 </div>
 </template>
 
 <script>
 	import { db } from '@/firebase'
-	import { mapGetters, mapActions } from 'vuex'
+	import { mapGetters } from 'vuex'
 
 	export default {
 		name: 'home',
 		metaInfo: {
 			title: 'Followed Users',
 		},
-		firebase() {
+		data() {
 			return {
-				users: {
-					source: db.ref(`users`),
-					asObject: true
-				},
-				broadcasting: {
-					source: db.ref(`broadcast`),
-					asObject: true
-				}
+				following: []
+			}
+		},
+		async mounted() {
+			for(const key in this.userInfo.follow) {
+				let user = {};
+				user.key = key;
+
+				let getUser = db.ref(`users/${key}`);
+				user.username = await getUser.once('value').then(function(snapshot) {
+					return snapshot.val().username
+				});
+
+				let broadcasting = db.ref(`broadcast/${key}/live`);
+				user.live = await broadcasting.once('value').then(function(snapshot) {
+					return snapshot.val()
+				});
+
+				this.following.push(user);
 			}
 		},
 		computed: {
 			...mapGetters([
 				'userInfo',
 			]),
-		},
-		methods: {
-		
-		},
+		}
 	}
 </script>
 
@@ -57,6 +66,10 @@
 
 		ul.entities {
 			li {
+				.live {
+					line-height: 20px;
+					padding: 0px 10px;
+				}
 				i {
 					margin-top: 5px;
 				}

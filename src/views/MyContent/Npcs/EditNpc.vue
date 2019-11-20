ac36d0ea2176503517ce74a0168515082432f3e1
Harm Manders <harmmanders@gmail.com>
2019-11-20 17:29:43 +0100

    mixin for hitdice string made, shown in edit npc

--- src/views/MyContent/Npcs/EditNpc.vue
+++ src/views/MyContent/Npcs/EditNpc.vue
@@ -241,7 +241,7 @@
 						<p class="validate red" v-if="errors.has('hp')">{{ errors.first('hp') }}</p>
 					</b-col>
 					<b-col class="col" v-if="quick == false">
-						<label for="hitdice">Hit Dice</label>
+						<label for="hitdice">Hit Dice ({{ hitDiceStr(npc) }})</small></label>
 						<b-form-input autocomplete="off" 
 							v-b-tooltip.hover title="Hit Dice"
 							type="text" 
@@ -525,9 +525,12 @@
 
 	import { db } from '@/firebase'
 	import { mapActions, mapGetters } from 'vuex'
+	import { general } from '@/mixins/general.js'
+
 
 	export default {
 		name: 'Npcs',
+		mixins: [general],
 		metaInfo: {
 			title: 'NPC\'s'
 		},

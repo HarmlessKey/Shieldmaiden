e1bec769500db7d68af1fcb4229bb208e800789c
Key <keyroos@gmail.com>
2019-11-20 18:20:34 +0100

    users in HK Table

--- src/views/Admin/Users.vue
+++ src/views/Admin/Users.vue
@@ -1,87 +1,79 @@
 <template>
-	<div class="container-fluid" v-if="users">
+	<div class="container-fluid">
 		<template v-if="!$route.params.id">
-		<Crumble />
-		<h1 class="mb-3"><i class="fas fa-users"></i> Users ( {{ Object.keys(users).length }}, {{ Object.keys(online).length }} online )</h1>
+			<Crumble />
+			<h1 class="mb-3">
+				<i class="fas fa-users"></i> Users 
+					<span v-if="isBusy" class="loader pl-5"></span>
+				<template v-else>
+					( {{ Object.keys(users).length }}, {{ Object.keys(online).length }} online )
+				</template>
+			</h1>
 
-			<b-row>
-				<b-col sm="8">
-					<b-input-group class="mb-3">
-						<input class="form-control" type="text" autocomplete="off" v-model="search" @keyup="searchCondition()" placeholder="Search users" />
-						<b-input-group-append>
-							<button class="btn" @click="searchCondition()"><i class="fas fa-search"></i></button>
-						</b-input-group-append>
-					</b-input-group>
-				</b-col>
-				<b-col class="text-right">
-					<h2>{{ Object.keys(users).length }} users.</h2>
-				</b-col>
-			</b-row>
+			<b-input-group class="mb-3">
+				<input class="form-control" type="text" autocomplete="off" v-model="search" @keyup="searchCondition()" placeholder="Search users" />
+				<b-input-group-append>
+					<button class="btn" @click="searchCondition()"><i class="fas fa-search"></i></button>
+				</b-input-group-append>
+			</b-input-group>
+
 
 			<p v-if="noResult" class="red">{{ noResult }}</p>
 			<p v-if="searching && !noResult" class="green">{{ Object.keys(searchResults).length }} users found</p>
-			<div class="table-responsive">
-				<b-table 
-					:busy="isBusy"
-					:items="searchResults" 
-					:fields="fields"
-					:per-page="15"
-					:current-page="current"
-				>	
-					<!-- STATUS -->
-					<span slot="status" slot-scope="data">
-						<template v-if="data.value === 'online'">
-							<i :class="{ 'green': data.value == 'online', 'gray-hover': data.value == 'offline' }" class="fas fa-circle"></i>
-						</template>
-						<span v-else><i class="fas fa-circle gray-hover"></i></span>
-					</span>
 
-					<!-- USERNAME -->
-					<router-link :to="'/admin/users/' + data.item['.key']" slot="username" slot-scope="data">
-						<span v-if="data.value">{{ data.value }}</span>
-						<span v-else>UNDEFINED</span>
-					</router-link>
+			<HKtable
+				:items="searchResults"
+				:columns="fields"
+				:perPage="15"
+				:currentPage="current"
+				:loading="isBusy"
+			>
+				<span slot="status" slot-scope="data">
+					<template v-if="data.item.status === 'online'">
+						<i :class="{ 'green': data.item.status === 'online', 'gray-hover': data.item.status === 'offline' }" class="fas fa-circle"></i>
+					</template>
+					<span v-else><i class="fas fa-circle gray-hover"></i></span>
+				</span>
+
+				<router-link :to="'/admin/users/' + data.item['.key']" slot="username" slot-scope="data">
+					<span v-if="data.item.username">{{ data.item.username }}</span>
+					<span v-else>UNDEFINED</span>
+				</router-link>
 					
-					<!-- VOUCHER -->
-					<span slot="voucher" slot-scope="data" v-if="data.value">
-						<span 
-							v-if="tiers[data.value.id]"
-							:class="{
-								'blue': tiers[data.value.id].name == 'Folk Hero',
-								'purple': tiers[data.value.id].name == 'Noble',
-								'orange': tiers[data.value.id].name == 'Deity'
-						}">
-							{{ tiers[data.value.id].name }}</span>
-					</span>
+				<span slot="voucher" slot-scope="data" v-if="data.item.voucher">
+					<i 
+						v-if="tiers[data.item.voucher.id]"
+						class="fas fa-ticket-alt"
+						:class="{
+							'blue': tiers[data.item.voucher.id].name == 'Folk Hero',
+							'purple': tiers[data.item.voucher.id].name == 'Noble',
+							'orange': tiers[data.item.voucher.id].name == 'Deity'
+					}"></i>
+				</span>
 
-					<!-- PATREON -->
-					<span slot="patreon" slot-scope="data" v-if="data.value">
-						<span v-if="data.value === 'Expired'" class="red">{{ data.value }}</span>
-						<span 
-							v-else
-							v-for="tier in data.value"
-							:key="tier"
-							class="tiers"
-							:class="{
-								'blue': tiers[tier].name == 'Folk Hero',
-								'purple': tiers[tier].name == 'Noble',
-								'orange': tiers[tier].name == 'Deity',
-								'red' : tiers[tier].name == 'Former'
-						}">
-							{{ tiers[tier].name }}</span>
-					</span>
+				<span slot="patreon" slot-scope="data" v-if="data.item.patreon">
+					<span v-if="data.item.patreon === 'Expired'" class="red">{{ data.item.patreon }}</span>
+					<i 
+						v-else
+						v-for="tier in data.item.patreon"
+						:key="tier"
+						class="fab fa-patreon"
+						:class="{
+							'blue': tiers[tier].name == 'Folk Hero',
+							'purple': tiers[tier].name == 'Noble',
+							'orange': tiers[tier].name == 'Deity',
+							'red' : tiers[tier].name == 'Former'
+					}"></i>
+				</span>
 
-					<!-- LIVE -->
-					<span slot="live" slot-scope="data" v-if="data.value" class="red">
-						Live
-					</span>
+				<span slot="live" slot-scope="data" v-if="data.item.live" class="red">
+					<i class="far fa-dot-circle"></i>
+				</span>
 
-					<!-- LOADER -->
-					<div slot="table-busy" class="loader">
-						<span>Loading users....</span>
-					</div>
-				</b-table>
-			</div>
+				<div slot="table-loading" class="loader">
+					<span>Loading users...</span>
+				</div>
+			</HKtable>
 		
 			<b-pagination v-if="!isBusy && Object.keys(searchResults).length > 15" align="center" :total-rows="Object.keys(searchResults).length" v-model="current" :per-page="15" />
 		</template>
@@ -94,16 +86,18 @@
 </template>
 
 <script>
-	import { db } from '@/firebase'
-	import Crumble from '@/components/crumble/Compendium.vue'
-	import User from '@/components/Admin/User.vue'
-	import { mapActions } from 'vuex'
+	import { db } from '@/firebase';
+	import Crumble from '@/components/crumble/Compendium.vue';
+	import User from '@/components/Admin/User.vue';
+	import { mapActions } from 'vuex';
+	import HKtable from '@/components/hk-components/hk-table.vue';
 
 	export default {
 		name: 'Users',
 		components: {
 			Crumble,
 			User,
+			HKtable
 		},
 		metaInfo: {
 			title: 'Admin | Users'
@@ -114,28 +108,32 @@
 				users: undefined,
 				current: 1,
 				fields: {
-          status: {
-						label: 'State',
+					status: {
+						maxContent: true,
 						sortable: true
 					},
-          username: {
-            label: 'Username',
-            sortable: true
+					username: {
+						label: 'Username',
+						truncate: true,
+						sortable: true
 					},
-          email: {
-            label: 'Email',
-            sortable: true
+					email: {
+						label: 'Email',
+						truncate: true,
+						sortable: true
 					},
 					voucher: {
-						label: 'Voucher',
+						label: '<i class="fas fa-ticket-alt"></i>',
+						maxContent: true,
 						sortable: true
 					},
 					patreon: {
-						label: 'Patreon',
+						label: '<i class="fab fa-patreon"></i>',
+						maxContent: true,
 						sortable: true
 					},
 					live: {
-						label: 'Live',
+						maxContent: true,
 						sortable: true
 					}
 				},
@@ -165,6 +163,7 @@
 
 					for(let key in users) {
 						users[key]['.key'] = key;
+						users[key].status = users[key].status ? users[key].status : 'offline';
 						let email = (users[key].patreon_email) ? users[key].patreon_email : users[key].email;
 
 						//Get Patreon

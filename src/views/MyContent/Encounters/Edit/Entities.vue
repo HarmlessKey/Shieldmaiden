e592ebdbeb99404c425656d73e663c24a0f6c6a1
Harm Manders <harmmanders@gmail.com>
2019-11-27 22:37:17 +0100

    fixed monsters array for table results

--- src/views/MyContent/Encounters/Edit/Entities.vue
+++ src/views/MyContent/Encounters/Edit/Entities.vue
@@ -1,71 +1,71 @@
 <template>
-    <div v-if="monsters">
-        <h3>Players</h3>
+	<div v-if="monsters">
+		<h3>Players</h3>
 
-        <!-- PLAYERS -->
-        <div class="players bg-gray mb-1" v-if="campaign.players">
-            <div v-for="(player, key) in campaign.players" 
-                :key="key"
-                @click="add(key, 'player', players[key].character_name)" 
-                v-b-tooltip.hover 
-                :title="'Add ' + players[key].character_name">
-                <div class="d-flex justify-content-left">
-                <template v-if="checkPlayer(key) < 0">
-                    <span v-if="players[key].avatar" class="img" :style="{ backgroundImage: 'url(\'' + players[key].avatar + '\')' }"></span>
-                    <span v-else class="img"><img src="@/assets/_img/styles/player.svg" /></span>
-                </template>
-                </div>
-            </div>
-
-            <div v-if="campaign && campaign.players === undefined">
-                <h3 class="red"><i class="fas fa-users"></i> No Players Yet</h3>
-                <p>Add players to your campaign first.</p>
-                <router-link :to="'/campaigns/' + campaignId" class="btn btn-block">Go to campaign</router-link>
-            </div>
-            <a v-else class="btn" @click="addAllPlayers()">Add all</a>
-        </div>
-        <p><small>Missing players? <router-link :to="'/campaigns/'+campaignId">Add them to your campaign first</router-link>.</small></p>
-		<hr>
-
-        <!-- MONSTERS -->
-		<h3>NPC's</h3>
-
-		<HKtable 
-			:items="monsters"
-			:columns="monsterFields"
-			:perPage="15"
-			:loading="loadingNpcs"
-			:search="['name', 'type']"
-		>
-			<template slot="name" slot-scope="data">
-				<a @click="setSlide({show: true, type: 'ViewEntity', data: data.row })" :class="{ 'green': data.row.custom}">
-                    {{ data.item }}
-                </a>
-			</template>
-			
-			<!-- ACTIONS -->
-			<div slot="actions" slot-scope="data">
-                <div class="monster-actions">
-                    <b-form-input class="multi_nr" autocomplete="off" v-b-tooltip.hover title="Add multiple npc's at once" type="number" min="1" name="name" placeholder="1" v-model="to_add[data.row['.key']]" />
-                    <a v-b-tooltip.hover title="Add with average HP" @click="multi_add(data.row['.key'], 'npc', data.row.name, data.row.custom)">
-                        <i class="fas fa-plus"></i>
-                    </a>
-                    <a v-b-tooltip.hover title="Add and roll HP" @click="multi_add(data.row['.key'], 'npc', data.row.name, data.row.custom, true)">
-                        <i class="fas fa-dice-d20"></i>
-                    </a>
-                </div>
-            </div>
-
-			<!-- LOADER -->
-			<div slot="table-loading" class="loader">
-				<span>Loading monsters...</span>
+		<!-- PLAYERS -->
+		<div class="players bg-gray mb-1" v-if="campaign.players">
+			<div v-for="(player, key) in campaign.players" 
+			:key="key"
+			@click="add(key, 'player', players[key].character_name)" 
+			v-b-tooltip.hover 
+			:title="'Add ' + players[key].character_name">
+			<div class="d-flex justify-content-left">
+				<template v-if="checkPlayer(key) < 0">
+					<span v-if="players[key].avatar" class="img" :style="{ backgroundImage: 'url(\'' + players[key].avatar + '\')' }"></span>
+					<span v-else class="img"><img src="@/assets/_img/styles/player.svg" /></span>
+				</template>
 			</div>
-		</HKtable>
-    </div>
+		</div>
+
+		<div v-if="campaign && campaign.players === undefined">
+			<h3 class="red"><i class="fas fa-users"></i> No Players Yet</h3>
+			<p>Add players to your campaign first.</p>
+			<router-link :to="'/campaigns/' + campaignId" class="btn btn-block">Go to campaign</router-link>
+		</div>
+		<a v-else class="btn" @click="addAllPlayers()">Add all</a>
+	</div>
+	<p><small>Missing players? <router-link :to="'/campaigns/'+campaignId">Add them to your campaign first</router-link>.</small></p>
+	<hr>
+
+	<!-- MONSTERS -->
+	<h3>NPC's</h3>
+
+	<HKtable 
+	:items="monsterArray"
+	:columns="monsterFields"
+	:perPage="15"
+	:loading="loadingNpcs"
+	:search="['name', 'type']"
+	>
+	<template slot="name" slot-scope="data">
+		<a @click="setSlide({show: true, type: 'ViewEntity', data: data.row })" :class="{ 'green': data.row.custom}">
+			{{ data.item }}
+		</a>
+	</template>
+
+	<!-- ACTIONS -->
+	<div slot="actions" slot-scope="data">
+		<div class="monster-actions">
+			<b-form-input class="multi_nr" autocomplete="off" v-b-tooltip.hover title="Add multiple npc's at once" type="number" min="1" name="name" placeholder="1" v-model="to_add[data.row['.key']]" />
+			<a v-b-tooltip.hover title="Add with average HP" @click="multi_add(data.row['.key'], 'npc', data.row.name, data.row.custom)">
+				<i class="fas fa-plus"></i>
+			</a>
+			<a v-b-tooltip.hover title="Add and roll HP" @click="multi_add(data.row['.key'], 'npc', data.row.name, data.row.custom, true)">
+				<i class="fas fa-dice-d20"></i>
+			</a>
+		</div>
+	</div>
+
+	<!-- LOADER -->
+	<div slot="table-loading" class="loader">
+		<span>Loading monsters...</span>
+	</div>
+</HKtable>
+</div>
 </template>
 
 <script>
-    import { db } from '@/firebase';
+	import { db } from '@/firebase';
 	import { mapActions, mapGetters } from 'vuex';
 	
 	import { dice } from '@/mixins/dice.js';
@@ -80,15 +80,15 @@
 		},
 		data() {
 			return {
-                campaignId: this.$route.params.campid,
-                encounterId: this.$route.params.encid,
+				campaignId: this.$route.params.campid,
+				encounterId: this.$route.params.encid,
 				user: this.$store.getters.getUser,
 				monsters: undefined,
-                loadingNpcs: true,
+				loadingNpcs: true,
 				auto_npcs: [],
 				viewNPC: [],
 				slide: this.$store.getters.getSlide,
-                to_add: {},
+				to_add: {},
 				typeFilter: [],
 				monsterFields: {
 					name: {
@@ -120,7 +120,7 @@
 			}),
 			this.fetchCampaign({
 				cid: this.campaignId, 
-            })
+			})
 
 			//GET NPCS
 			var monsters = db.ref(`monsters`);
@@ -140,24 +140,25 @@
 						monsters.push(customNpcs[key]);
 					}
 				});
-				this.monsters = Object.values(monsters);
+				this.monsterArray = Object.values(monsters);
+				this.monsters = monsters;
 				this.loadingNpcs = false;
 			});
-        },
-        computed: {
+		},
+		computed: {
 			...mapGetters([
 				'encounter',
 				'campaign',
 				'players',
 				'npcs',
-			]),
-        },
+				]),
+		},
 		methods: {
 			...mapActions([
 				'fetchEncounter',
-                'fetchCampaign',
-                'setSlide'
-			]),
+				'fetchCampaign',
+				'setSlide'
+				]),
 			multi_add(id,type,name,custom=false,rollHp=false) {
 				if (!this.to_add[id]) {
 					this.to_add[id] = 1
@@ -166,8 +167,8 @@
 					this.add(id,type,name,custom,rollHp)
 				}
 				this.to_add[id] = 1
-            },
-     		add(id, type, name, custom = false, rollHp = false) {
+			},
+			add(id, type, name, custom = false, rollHp = false) {
 				var entity = {
 					id: id,
 					name: name,
@@ -199,6 +200,7 @@
 					}
 					
 					if(custom == false) {
+						// console.log(this.monsters[id-1])
 						var npc_data = this.monsters[id];
 						entity.npc = 'api'
 						if(rollHp && npc_data.hit_dice) {
@@ -258,13 +260,13 @@
 						position: "centerTop"
 					});
 				}
-            },
+			},
 			addAllPlayers() {
 				for(let player in this.campaign.players) {
 					let name = this.players[player].character_name;
 					this.add(player, 'player', name)
 				}
-            },
+			},
 			checkPlayer(id) {
 				if(this.encounter.entities) {
 					return (Object.keys(this.encounter.entities).indexOf(id))
@@ -279,8 +281,8 @@
 <style lang="scss" scoped>
 // Remove arrows from number field
 input[type="number"]::-webkit-outer-spin-button, input[type='number']::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
+	-webkit-appearance: none;
+	margin: 0;
 }
 .monster-actions {
 	display: flex;

711925270a55005965739479c3921047616dc997
Unknown <keyroos@gmail.com>
2020-01-10 19:56:33 +0100

    Added links to Discord

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,12 +16,13 @@
 				
 			</div>
 			<div class="d-flex justify-content-end">
-				<!-- <div class="area d-flex justify-content-end">
-					<a href="https://www.patreon.com/harmlesskey" target="_blank" v-b-tooltip.hover title="Patreon" class="icon"><i class="fab fa-patreon patreon-red"></i></a>
+				<div class="area d-flex justify-content-end">
+					<a href="https://discord.gg/fhmKBM7" target="_blank" v-b-tooltip.hover title="Discord" class="icon"><i class="fab fa-discord"></i></a>
+					<!-- <a href="https://www.patreon.com/harmlesskey" target="_blank" v-b-tooltip.hover title="Patreon" class="icon"><i class="fab fa-patreon patreon-red"></i></a>
 					<a href="https://www.facebook.com/harmlesskey" target="_blank" v-b-tooltip.hover title="Facebook" class="icon"><i class="fab fa-facebook-f"></i></a>
 					<a href="https://www.reddit.com/r/HarmlessKey" target="_blank" v-b-tooltip.hover title="Reddit" class="icon"><i class="fab fa-reddit-alien"></i></a>
-					<router-link v-if="user" class="icon" to="/feedback" v-b-tooltip.hover title="Give Feedback"><i class="fas fa-comment-alt"></i></router-link>
-				</div> -->
+					<router-link v-if="user" class="icon" to="/feedback" v-b-tooltip.hover title="Give Feedback"><i class="fas fa-comment-alt"></i></router-link> -->
+				</div>
 				<div class="area d-flex justify-content-end">
 					<a class="icon" 
 						v-b-tooltip.hover 
--- src/views/Updates.vue
+++ src/views/Updates.vue
@@ -2,8 +2,12 @@
 	<div class="grid">
 		<div class="content">
 			<h1>Updates</h1>
-			<p>Check our latest updates on Trello.</p>
-			<a href="https://trello.com/b/FGyjhDOt/harmless-key" target="_blank"><i class="fab fa-trello"></i> Harmless Key Trello.</a>
+			<p>Check the latest updates on our Discord.</p>
+
+			<a href="https://discord.gg/gCXJZCs" target="_blank" class="large-link">
+				<div class="icon bg-discord-purple"><i class="fab fa-discord white"></i></div>
+				<div class="text">Harmless Key Discord</div>
+			</a>
 		</div>
 		<Footer />
 	</div>

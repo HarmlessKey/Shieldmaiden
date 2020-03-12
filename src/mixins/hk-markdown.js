d53564328e9892aea9c8cd0541746bed0b75b405
Harm Manders <harmmanders@gmail.com>
2020-03-12 18:14:08 +0100

    Parse unordered lists

--- src/mixins/hk-markdown.js
+++ src/mixins/hk-markdown.js
@@ -16,14 +16,22 @@ export const hkMarkdown = {
 					regex: /(\*|_)([^\s\*].*?)\1/g,
 					repla: '<i>$2</i>'
 				},
+				{ // Find and place UL
+					regex: /(?:\-\s(.*?)\n)+/g,
+					repla: "<ul>$&</ul>"
+				},
 				{
+					regex: /\-\s(.*?)\n/g,
+					repla: "<li>$1</li>"
+				},
+				{ // Parse paragraphs
 					regex: /(.+)\n{2}(.+)/g,
 					repla: "$1</p><p>$2"
 				},
-				{
+				{ // Parse breaks
 					regex: /\n/g,
 					repla: "<br/>"
-				}
+				},
 			];
 
 			rules.forEach(function(rule) {

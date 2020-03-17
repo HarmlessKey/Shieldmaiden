8070aa4c3bb91230a8707b4da36b516bb78c2394
Harm Manders <harmmanders@gmail.com>
2020-03-17 18:35:01 +0100

    Added display options in edit npc

--- src/components/slides/encounter/EditNpc.vue
+++ src/components/slides/encounter/EditNpc.vue
@@ -98,41 +98,28 @@
 
 		<template>
 			<hr>
-			<h2 class="mb-0">Display</h2>
-			<b-row class="my-2">
-				<b-col class="text-center" v-if="entity.npc">
-					<label>Name</label>
-					<b-form-checkbox
-						class="text-center"
-						name="nameHidden" 
-						v-model="entity.nameHidden"></b-form-input>
-				</b-col>
+			<h2 class="mb-0">Display Override</h2>
 
-				<!-- <b-col class="text-center" v-if="entity.npc">
-					<label>AC</label>
-					<b-form-input 
-						class="text-center"
-						type="number" 
-						name="maxHp" 
-						min="1"
-						v-model="entity.maxHp"
-						placeholder="Maximum Hit Points"></b-form-input>
-				</b-col> -->
+			<ul class="settings">
+				<li v-for="(setting, key) in npcsOptions" class="d-flex justify-content-between" :key="key">
+					<span><i :class="setting.icon + ' gray-hover'"></i> {{ setting.name }}</span>
+					<div>
 
-				<!-- <b-col class="text-center">
-					<label>Cur HP</label>
-					<b-form-input 
-						class="text-center"
-						type="number" 
-						name="maxHp" 
-						min="1"
-						v-model="entity.curHp"
-						v-validate="'required|numeric'"
-						data-vv-as="Current HP"
-						placeholder="Current Hit Points"></b-form-input>
-						<p class="validate red" v-if="errors.has('curHp')">{{ errors.first('curHp') }}</p>
-				</b-col> -->
-			</b-row>
+						<a v-for="option in setting.options"
+							v-b-tooltip.hover 
+							:title="[ isActive(key, option) ? option.name : option.action ]" 
+							:key="option.name" 
+							@click="setSetting(key, option.value)" class="ml-2"
+							:class="[ isActive(key, option) ? option.color : 'gray-light' ]">
+								<span class="d-none d-md-inline mr-1">
+									<template v-if="isActive(key, option)">{{ option.name }}</template>
+									<template v-else>{{ option.action }}</template>
+								</span>
+								<i :class="option.icon"></i>
+						</a>
+					</div>
+				</li>
+			</ul>
 		</template>
 
 		<button class="btn btn-block my-3" @click="edit()">Save</button>
@@ -159,15 +146,51 @@
 				entity: undefined,
 				userId: this.$store.getters.getUser.uid,
 				campaignId: this.$route.params.campid,
-				encounterId: this.$route.params.encid
-			}
-		},
-		firebase() {
-			return {
-				// entity: {
-				// 	source:	db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${this.entityKey}`),
-				// 	asObject: true
-				// }
+				encounterId: this.$route.params.encid,
+				npcSettings: undefined,
+				npcsOptions: {
+					'name': { 
+						entity: 'npc',
+						name: 'Name', 
+						icon: 'fas fa-helmet-battle',
+						options: {
+							0: { value: false, name: 'Hidden', action: 'Hide', icon: 'fas fa-eye-slash', color: 'red' },
+							// 1: { value: 'obscured', name: 'Obsc', action: 'Obsc', icon: 'fas fa-question-circle', color: 'orange' },
+							1: { value: true, name: 'Shown', action: 'Show', icon: 'fas fa-eye', color: 'green', settings_default: true },
+							// 2: { value: undefined, name: 'Default', action: 'Default', icon: 'fas fa-sparkles', color: 'blue'}
+						}
+					},
+					'health': { 
+						entity: 'npc',
+						name: 'Health', 
+						icon: 'fas fa-heart',
+						options: {
+							0: { value: false, name: 'Hidden', action: 'Hide', icon: 'fas fa-eye-slash', color: 'red', settings_default: true },
+							1: { value: 'obscured', name: 'Obsc', action: 'Obsc', icon: 'fas fa-question-circle', color: 'orange' },
+							2: { value: true, name: 'Shown', action: 'Show', icon: 'fas fa-eye', color: 'green' },
+							// 3: { value: undefined, name: 'Default', action: 'Default', icon: 'fas fa-sparkles', color: 'blue'}
+						}
+					},
+					'ac': { 
+						entity: 'npc',
+						name: 'Armor Class', 
+						icon: 'fas fa-shield',
+						options: {
+							0: { value: false, name: 'Hidden', action: 'Hide', icon: 'fas fa-eye-slash', color: 'red', settings_default: true },
+							1: { value: true, name: 'Shown', action: 'Show', icon: 'fas fa-eye', color: 'green' },
+							// 2: { value: undefined, name: 'Default', action: 'Default', icon: 'fas fa-sparkles', color: 'blue'}
+						}
+					},
+					// 'conditions': { 
+					// 	entity: 'npc',
+					// 	name: 'Conditions', 
+					// 	icon: 'fas fa-skull-crossbones',
+					// 	options: {
+					// 		0: { value: false, name: 'Hidden', action: 'Hide', icon: 'fas fa-eye-slash', color: 'red' },
+					// 		1: { value: undefined, name: 'Shown', action: 'Show', icon: 'fas fa-eye', color: 'green' },
+					// 	}
+					// },
+				},
 			}
 		},
 		computed: {
@@ -175,12 +198,19 @@
 				'demoEntities'
 			])	
 		},
+
 		mounted() {
 			if(!this.demo) {
-				var entity = db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${this.entityKey}`);
-				entity.on('value', async (snapshot) => {
+				var entity_ref = db.ref(`encounters/${this.userId}/${this.campaignId}/${this.encounterId}/entities/${this.entityKey}`);
+				entity_ref.on('value', async (snapshot) => {
 					this.entity = snapshot.val();
 				});
+
+				var npcSettings_ref = db.ref(`settings/${this.userId}/track/npc`);
+				npcSettings_ref.on('value', async (snapshot) => {
+					this.npcSettings = snapshot.val();
+				});
+
 			} else {
 				this.entity = this.demoEntities[this.entityKey];
 			}

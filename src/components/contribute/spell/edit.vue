b3da3f7302b8c32ecf9cd26c675f473aa944a44c
Harm Manders <harmmanders@gmail.com>
2020-03-26 13:21:00 +0100

    Add text top of page on change

--- src/components/contribute/spell/edit.vue
+++ src/components/contribute/spell/edit.vue
@@ -63,6 +63,7 @@
 							<i class="fad fa-treasure-chest green"></i>
 							<span class="d-none d-md-inline ml-1 green">Save</span>
 						</a>
+						<span v-if="unsaved_changes" class="red unsaved_changes">There are unsaved changes in the spell</span>
 					</div>
 					<basic-info v-model='spell' :levels='levels' @validation="setValidators" />
 					<!-- SPELL ACTIONS -->
@@ -75,269 +76,278 @@
 </template>
 
 <script>
-	import { db } from '@/firebase'
-	import Crumble from '@/components/crumble/Compendium.vue'
-	import basicInfo from '@/components/contribute/spell/forms/basic-info.vue'
-	import spellActions from '@/components/contribute/spell/forms/spell-actions.vue'
-	import { general } from '@/mixins/general.js'
-	import { mapGetters } from 'vuex'
+import { db } from '@/firebase'
+import Crumble from '@/components/crumble/Compendium.vue'
+import basicInfo from '@/components/contribute/spell/forms/basic-info.vue'
+import spellActions from '@/components/contribute/spell/forms/spell-actions.vue'
+import { general } from '@/mixins/general.js'
+import { mapGetters } from 'vuex'
 
-	export default {
-		name: 'SpellEdit',
-		components: {
-			Crumble,
-			basicInfo,
-			spellActions,
-		},
-		mixins: [general],
-		props: ['id'],
-		metaInfo() {
-			return {
-				title: this.old_spell.name + ' | D&D 5th Edition',
-				meta: [
-          { vmid: 'description', name: 'description', content: 'D&D 5th Edition Spell: ' + this.old_spell.name }
-        ]
-			}
-		},
-		beforeMount() {
-			//Because the component is loaded
-			//in another view, the scroll needs to be reset to 0
-			window.scrollTo(0,0);
-		},
-		data() {
-			return {
-				loading: true,
-				levels: ["Cantrip",
-					"1st","2nd","3rd",
-					"4th","5th","6th",
-					"7th","8th","9th"],
-				validators: {},
-			}
-		},
-		computed: {
-			...mapGetters([
-				'tier',
-			]),
-		},
-		mounted() {
-			this.$nextTick(function() {
-				if ($('ins').length > 0) {
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				}
-			})
-		},
-		firebase() {
-			return {
-				spell: {
-					source: db.ref(`new_spells/${this.id}`),
-					asObject: true,
-					readyCallback: () => this.loading = false
-				},
-				old_spell: {
-					source: db.ref(`spells/${this.id}`),
-					asObject: true,
-					readyCallback: () => this.loading = false
-				}
-			}
-		},
-		methods: {
-			parse_old_spell() {
-				// Parse values from old_spell object to new spell object
-
-				
-				// Parse simple values
-				// this.$set(this.spell, 'name', this.old_spell.name);
-				this.spell.name = this.old_spell.name;
-				this.spell.school = this.old_spell.school.name.toLowerCase();
-				this.spell.ritual = (this.old_spell.ritual == 'yes') ? true : false;
-				this.spell.level = (this.old_spell.level == -1) ? 0 : this.old_spell.level;
-				this.spell.level_scaling = (this.old_spell.higher_level) ? undefined : "None";
-				
-				// Parse range options
-				if (parseInt(this.old_spell.range)) {
-					this.spell.range_type = "Ranged";
-					let range_list = this.old_spell.range.split(' ');
-					let rangeN = parseInt(range_list[0]);
-					// Parse miles to feet
-					if (range_list[1].includes('mile')) {
-						rangeN *= 5280;
-					}
-					this.spell.range = rangeN;
-				} else {
-					this.spell.range_type = this.old_spell.range;
-					delete this.spell.range;
-				}
-
-				// Parse casting time
-				let cast_time = this.old_spell.casting_time.split(' ');
-				this.spell.cast_time_nr = parseInt(cast_time[0]);
-				let cast_type =  cast_time[1];
-																	 
-				if (cast_type[cast_type.length -1] == 's') {
-					cast_type = cast_type.substring(0, cast_type.length - 1);
-				}
-				this.spell.cast_time_type = cast_type;
-				delete this.spell.casting_time;
-
-				// Parse components
-				this.spell.components = {'verbal': false,'somatic': false ,'material': false};
-				for (let i in this.old_spell.components) {
-					if (this.old_spell.components[i] == "V") {this.spell.components.verbal = true}
-					if (this.old_spell.components[i] == "S") {this.spell.components.somatic = true}
-					if (this.old_spell.components[i] == "M") {this.spell.components.material = true}
-				}
-				if (this.old_spell.material) {
-					this.spell.material_description = this.parse_spell_str(this.old_spell.material);
-					delete this.spell.material;
-				}
-
-				// Parse duration
-				// If a number in duration duration = concentration or time
-				if (/\d/.test(this.old_spell.duration)) {
-					let duration_list = this.old_spell.duration.split(' ');
-					if (this.old_spell.concentration == 'yes') {
-						this.spell.duration_type = "Concentration";
-						duration_list = duration_list.slice(2);
-					} else {
-						this.spell.duration_type = "Time";
-					}
-					
-					// Find duration time number and scale
-					this.spell.duration_n = parseInt(duration_list[0]);
-					// Calculate time scale
-					let scale = duration_list[1];
-					scale = scale.charAt(0).toUpperCase() + scale.substring(1);
-					if (scale[scale.length -1] == 's') {
-						scale = scale.substring(0, scale.length -1);
-					}
-					this.spell.duration_scale = scale;
-
-				} else {
-					let duration_list = this.old_spell.duration.split(' ');
-					this.spell.duration_type = duration_list.map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
-				}
-
-				// Parse Description
-				this.spell.description = "";
-				// console.log(this.old_spell)
-				for (let i in this.old_spell.desc) {
-					// console.log((i))
-					// console.log((i !== '0'))
-					if (i != 0)
-						this.spell.description += "\n\n"; // Add white line before each paragraph after first
-					this.spell.description += this.parse_spell_str(this.old_spell.desc[i]);
-				}
-
-				this.spell.higher_level = "";
-				for (let i in this.old_spell.higher_level) {
-					this.spell.higher_level += this.parse_spell_str(this.old_spell.higher_level[i]);
-				}
-
-				// Parse classes
-				let classes = [];
-				for (let index in this.old_spell.classes) {
-					classes.push(this.old_spell.classes[index].name);
-				}
-				this.spell.classes = classes;
-
-				// Source book
-				this.spell.source = this.old_spell.page;
-
-				this.store_spell();
-
-				// Clean up spell object
-				// delete this.spell.concentration
-				// delete this.spell.duration
-				// delete this.spell.higher_level
-			},
-			parse_spell_str(text) {
-				// console.log(text);
-				// map to replace weird character with real character 
-				let rules = [
-					{
-						regex: /â€™/g,
-						replacement: '\'',
-					},
-					{
-						regex: /â€�/g,
-						replacement: '\"'
-					},
-					{
-						regex: /â€œ/g,
-						replacement: '\"'
-					},
-				];
-
-				rules.forEach(function(rule) {
-					text = text.replace(rule.regex, rule.replacement);
-				});
-
-				return text.trim();
-			},
-			update() {
-				this.$forceUpdate();
-			},
-			setValidators(validators) {
-				// Receives validator lists from basic info and spell actions
-				// console.log("set validate called root")
-				// console.log(validators)
-				// validators.forEach( validator => {
-
-				// })
-				for (let v in validators) {
-					this.validators[v] = validators[v];
-					// this.validators.push(validator)
-
-				}
-				console.log(this.validators);
-
-				// this.validation = validate;
-			},
-			async validate_validators() {
-				// loops through all available validators to check if the forms
-				// are all valid. This happens async.
-				for (let v in this.validators) {
-					let validator = this.validators[v];
-					 let temp = await validator.validateAll()
-					 if (temp == false)
-					 	return false;
-				}
-				return true;
-			},
-			async store_spell() {
-				delete this.spell['.value'];
-				delete this.spell['.key'];
-				this.spell.changed = true;
-				this.spell.checked = false;
-
-				if(this.spell.cast_time_nr) {
-					parseInt(this.spell.cast_time_nr);
-				}
-				if(this.spell.duration_n) {
-					parseInt(this.spell.duration_n);
-				}
-
-				
-				if (await this.validate_validators() === true) {
-				// this.validation.validateAll().then((result) => {
-					// if (result) {
-					console.log("Validated");
-					db.ref(`new_spells/${this.id}`).set(this.spell);
-					this.$snotify.success('Spell Saved.', 'Critical hit!', {
-						position: "rightTop"
-					});
-					// this.$router.replace('/players')
-				} else {
-					console.log("Not validated");
-					this.$snotify.error('Form Not Valid', 'Critical miss!', {
-						position: "rightTop"
-					});
-				}
-				// })
-			},
-			
+export default {
+	name: 'SpellEdit',
+	components: {
+		Crumble,
+		basicInfo,
+		spellActions,
+	},
+	mixins: [general],
+	props: ['id'],
+	metaInfo() {
+		return {
+			title: this.old_spell.name + ' | D&D 5th Edition',
+			meta: [
+        { vmid: 'description', name: 'description', content: 'D&D 5th Edition Spell: ' + this.old_spell.name }
+      ]
 		}
-	}
+	},
+	beforeMount() {
+		//Because the component is loaded
+		//in another view, the scroll needs to be reset to 0
+		window.scrollTo(0,0);
+	},
+	data() {
+		return {
+			loading: true,
+			levels: ["Cantrip",
+				"1st","2nd","3rd",
+				"4th","5th","6th",
+				"7th","8th","9th"],
+			validators: {},
+			spell: {},
+			unsaved_changes: false,
+		}
+	},
+	computed: {
+		...mapGetters([
+			'tier',
+		]),
+	},
+	mounted() {
+		this.$nextTick(function() {
+			if ($('ins').length > 0) {
+				(adsbygoogle = window.adsbygoogle || []).push({});
+			}
+		})
+	},
+	firebase() {
+		return {
+			spell: {
+				source: db.ref(`new_spells/${this.id}`),
+				asObject: true,
+				readyCallback: () => this.loading = false
+			},
+			old_spell: {
+				source: db.ref(`spells/${this.id}`),
+				asObject: true,
+				readyCallback: () => this.loading = false
+			}
+		}
+	},
+	methods: {
+		parse_old_spell() {
+			// Parse values from old_spell object to new spell object
+			console.log("In parse old spell")
+			
+			// Parse simple values
+			// this.$set(this.spell, 'name', this.old_spell.name);
+			this.spell.name = this.old_spell.name;
+			this.spell.school = this.old_spell.school.name.toLowerCase();
+			this.spell.ritual = (this.old_spell.ritual == 'yes') ? true : false;
+			this.spell.level = (this.old_spell.level == -1) ? 0 : this.old_spell.level;
+			this.spell.level_scaling = (this.old_spell.higher_level) ? undefined : "None";
+			
+			// Parse range options
+			if (parseInt(this.old_spell.range)) {
+				this.spell.range_type = "Ranged";
+				let range_list = this.old_spell.range.split(' ');
+				let rangeN = parseInt(range_list[0]);
+				// Parse miles to feet
+				if (range_list[1].includes('mile')) {
+					rangeN *= 5280;
+				}
+				this.spell.range = rangeN;
+			} else {
+				this.spell.range_type = this.old_spell.range;
+				delete this.spell.range;
+			}
+
+			// Parse casting time
+			let cast_time = this.old_spell.casting_time.split(' ');
+			this.spell.cast_time_nr = parseInt(cast_time[0]);
+			let cast_type =  cast_time[1];
+																 
+			if (cast_type[cast_type.length -1] == 's') {
+				cast_type = cast_type.substring(0, cast_type.length - 1);
+			}
+			this.spell.cast_time_type = cast_type;
+			delete this.spell.casting_time;
+
+			// Parse components
+			this.spell.components = {'verbal': false,'somatic': false ,'material': false};
+			for (let i in this.old_spell.components) {
+				if (this.old_spell.components[i] == "V") {this.spell.components.verbal = true}
+				if (this.old_spell.components[i] == "S") {this.spell.components.somatic = true}
+				if (this.old_spell.components[i] == "M") {this.spell.components.material = true}
+			}
+			if (this.old_spell.material) {
+				this.spell.material_description = this.parse_spell_str(this.old_spell.material);
+				delete this.spell.material;
+			}
+
+			// Parse duration
+			// If a number in duration duration = concentration or time
+			if (/\d/.test(this.old_spell.duration)) {
+				let duration_list = this.old_spell.duration.split(' ');
+				if (this.old_spell.concentration == 'yes') {
+					this.spell.duration_type = "Concentration";
+					duration_list = duration_list.slice(2);
+				} else {
+					this.spell.duration_type = "Time";
+				}
+				
+				// Find duration time number and scale
+				this.spell.duration_n = parseInt(duration_list[0]);
+				// Calculate time scale
+				let scale = duration_list[1];
+				scale = scale.charAt(0).toUpperCase() + scale.substring(1);
+				if (scale[scale.length -1] == 's') {
+					scale = scale.substring(0, scale.length -1);
+				}
+				this.spell.duration_scale = scale;
+
+			} else {
+				let duration_list = this.old_spell.duration.split(' ');
+				this.spell.duration_type = duration_list.map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+			}
+
+			// Parse Description
+			this.spell.description = "";
+			// console.log(this.old_spell)
+			for (let i in this.old_spell.desc) {
+				// console.log((i))
+				// console.log((i !== '0'))
+				if (i != 0)
+					this.spell.description += "\n\n"; // Add white line before each paragraph after first
+				this.spell.description += this.parse_spell_str(this.old_spell.desc[i]);
+			}
+
+			this.spell.higher_level = "";
+			for (let i in this.old_spell.higher_level) {
+				this.spell.higher_level += this.parse_spell_str(this.old_spell.higher_level[i]);
+			}
+
+			// Parse classes
+			let classes = [];
+			for (let index in this.old_spell.classes) {
+				classes.push(this.old_spell.classes[index].name);
+			}
+			this.spell.classes = classes;
+
+			// Source book
+			this.spell.source = this.old_spell.page;
+
+			// this.store_spell();
+
+			// Clean up spell object
+			// delete this.spell.concentration
+			// delete this.spell.duration
+			// delete this.spell.higher_level
+		},
+		parse_spell_str(text) {
+			// console.log(text);
+			// map to replace weird character with real character 
+			let rules = [
+				{
+					regex: /â€™/g,
+					replacement: '\'',
+				},
+				{
+					regex: /â€�/g,
+					replacement: '\"'
+				},
+				{
+					regex: /â€œ/g,
+					replacement: '\"'
+				},
+			];
+
+			rules.forEach(function(rule) {
+				text = text.replace(rule.regex, rule.replacement);
+			});
+
+			return text.trim();
+		},
+		update() {
+			this.$forceUpdate();
+		},
+		setValidators(validators) {
+			// Receives validator lists from basic info and spell actions
+			// console.log(validators);
+			for (let v in validators) {
+				this.validators[v] = validators[v];
+				// this.validators.push(validator)
+
+			}
+			// this.validation = validate;
+		},
+		async validate_validators() {
+			// loops through all available validators to check if the forms
+			// are all valid. This happens async.
+			for (let v in this.validators) {
+				let validator = this.validators[v];
+				 let temp = await validator.validateAll()
+				 if (temp == false)
+				 	return false;
+			}
+			return true;
+		},
+
+		async store_spell() {
+			delete this.spell['.value'];
+			delete this.spell['.key'];
+			this.spell.changed = true;
+			this.spell.checked = false;
+
+			if(this.spell.cast_time_nr) {
+				parseInt(this.spell.cast_time_nr);
+			}
+			if(this.spell.duration_n) {
+				parseInt(this.spell.duration_n);
+			}
+
+			
+			if (await this.validate_validators() === true) {
+			// this.validation.validateAll().then((result) => {
+				// if (result) {
+				console.log("Validated");
+				db.ref(`new_spells/${this.id}`).set(this.spell);
+				this.$snotify.success('Spell Saved.', 'Critical hit!', {
+					position: "rightTop"
+				});
+				this.validators = {};
+				this.unsaved_changes = false;
+				// this.$router.replace('/players')
+			} else {
+				console.log("Not validated");
+				this.$snotify.error('Form Not Valid', 'Critical miss!', {
+					position: "rightTop"
+				});
+			}
+			// })
+		},
+	},
+	watch: {
+		spell: {
+			// immediate: true,
+			deep: true,
+			handler() {
+				// Emits validation on every change
+				console.log("Change in object");
+				this.unsaved_changes = true;
+			},
+		}
+	},
+}
 </script>
 
 <!-- <style src="vue-multiselect/dist/vue-multiselect.min.css"></style> -->

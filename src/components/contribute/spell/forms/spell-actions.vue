4fe5c0ecddc219c08354701b9bc36a0a4a8be34f
Harm Manders <harmmanders@gmail.com>
2020-06-04 13:19:03 +0200

    Spell Action Table columns added and styled

--- src/components/contribute/spell/forms/spell-actions.vue
+++ src/components/contribute/spell/forms/spell-actions.vue
@@ -27,17 +27,32 @@
 					:columns="columns"
 					:items="spell.actions"
 				>
+					<template slot="type" slot-scope="data">
+						{{ data.item.capitalizeEach() }}						
+					</template>
+					<template slot="free" slot-scope="data">
+						<!-- {{ data.item }} -->
+
+						<!-- {{ data.item }} -->
+						<i v-if="!data.item" class="far fa-check green"></i>
+						<i v-else class="far fa-times red"></i>
+						<!-- {{ data.item ? '<i class="far fa-code-check"></i>' : '<i class="far fa-code-times"></i>' }} -->
+					</template>
+					<template slot="seperate" slot-scope="data">
+						<i v-if="data.item" class="far fa-check green"></i>
+						<i v-else class="far fa-times red"></i>
+					</template>
 					<template slot="modifiers" slot-scope="data">
-						{{ data.item ? data.item.length : "" }}
+						{{ data.item ? data.item.length : "0" }}
 					</template>
 					<template slot="conditions" slot-scope="data">
-						{{ data.item ? data.item.length : "" }}
+						{{ data.item ? data.item.length : "0" }}
 					</template>
 					<template slot="notifications" slot-scope="data">
-						{{ data.item ? data.item.length : "" }}
+						{{ data.item ? data.item.length : "0" }}
 					</template>
 					<template slot="effects" slot-scope="data">
-						{{ data.item ? data.item.length : "" }}
+						{{ data.item ? data.item.length : "0" }}
 					</template>
 					<div slot="actions" class="actions" slot-scope="data">
 						<a @click="edit_action(data.index)">
@@ -75,21 +90,44 @@ export default {
 				type: {
 					label: "Type"
 				},
+				name: {
+					label: "Name"
+				},
+				free: {
+					label: '<i class="far fa-book-spells"></i></span>',
+					tooltip: "Consumes Spell Slot",
+					center: true,
+					maxContent: true,
+				},
+				seperate: {
+					label: '<i class="far fa-code-branch"></i>',
+					tooltip: "Seperate Spell Action",
+					center: true,
+					maxContent: true,
+				},
 				modifiers: {
 					label: '<i class="fas fa-dice-d20"></i>',
-					center: true
+					tooltip: "Modifier",
+					center: true,
+					maxContent: true,
 				},
 				conditions: {
 					label: '<i class="fas fa-flame"></i>',
-					center: true
+					tooltip: "Conditions",
+					center: true,
+					maxContent: true,
 				},
 				notifications: {
 					label: '<i class="fas fa-bell"></i>',
-					center: true
+					tooltip: "Notification",
+					center: true,
+					maxContent: true,
 				},
 				effects: {
 					label: '<i class="fas fa-hand-holding-magic"></i>',
-					center: true
+					tooltip: "Effects",
+					center: true,
+					maxContent: true,
 				},	
 				actions: {
 					label: '<i class="far fa-ellipsis-h"></i>',

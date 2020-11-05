4eee176de6d662620f9d0acd2d1695d4cb43a07f
Unknown <keyroos@gmail.com>
2020-11-05 12:40:01 +0100

    New damage and healing done notifications

--- src/quasar.js
+++ src/quasar.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 
 import './styles/quasar.scss'
 import '@quasar/extras/material-icons/material-icons.css'
-import { Quasar } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 
 Vue.use(Quasar, {
-  config: {},
+  config: {
+  },
   components: { /* not needed if importStrategy is not 'manual' */ },
   directives: { /* not needed if importStrategy is not 'manual' */ },
   plugins: {
+    Notify
   }
  })

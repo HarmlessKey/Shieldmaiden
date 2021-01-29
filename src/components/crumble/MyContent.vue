30e704ae850ffc8765d634b10170433dc84fcc73
Unknown <keyroos@gmail.com>
2021-01-29 11:17:47 +0100

    content crumble fixed

--- src/components/crumble/MyContent.vue
+++ src/components/crumble/MyContent.vue
@@ -1,24 +1,4 @@
 <template>
-	<!-- <div class="d-none d-md-block">
-		<ul class="crumble d-flex justify-content-left">
-			<li>
-				<router-link to="/">Home</router-link>
-			</li>
-			<li>
-				<i class="fas fa-chevron-right mr-2"></i>
-				<router-link :to="$route.meta.basePath">{{ $route.meta.title }}</router-link>
-			</li>
-			<li v-if="campaignId">
-				<i class="fas fa-chevron-right mr-2"></i>
-				<router-link v-if="$route.name == 'EditEncounter' || $route.name == 'Encounters'" :to="'/encounters/' + campaignId">Encounters</router-link>
-				<span v-if="$route.name == 'Edit Campaign'">{{ $route.name }}</span>
-			</li>
-			<li v-if="encounterId">
-				<i class="fas fa-chevron-right mr-2"></i>
-				Edit encounter
-			</li>
-		</ul>
-	</div> -->
 	<div>
 		<q-breadcrumbs>
 			<template v-slot:separator>
@@ -29,10 +9,10 @@
 			<q-breadcrumbs-el icon="fas fa-home-alt" to="/home" />
 			<q-breadcrumbs-el label="Content" to="/" />
 			<q-breadcrumbs-el :label="$route.meta.title" :to="$route.meta.basePath" />
-			<templat v-if="campaignId">
+			<template v-if="campaignId">
 				<q-breadcrumbs-el v-if="$route.name == 'EditEncounter' || $route.name == 'Encounters'" :to="'/encounters/' + campaignId" label="Encounters" />
 				<q-breadcrumbs-el v-if="$route.name == 'Edit Campaign'" :label="$route.name.capitalize()" />
-			</templat>
+			</template>
 			<q-breadcrumbs-el v-if="encounterId" label="Edit encounter" />
 		</q-breadcrumbs>
 		<hr>

c6b1049bcfb80f4089a16d2e59ec965a809fa1e2
Unknown <keyroos@gmail.com>
2022-01-28 11:08:59 +0100

    Mobile HK ad

--- src/views/View.vue
+++ src/views/View.vue
@@ -3,6 +3,20 @@
 	<div class="content">
 		<Crumble />
 		<OverEncumbered v-if="show_overencumbered" />
+		<hk-card v-if="show_mobile_ad" class="ad">
+			<div class="card-body">
+				<img src="../assets/_img/logo/logo-icon-cyan.svg" alt="Harmless Key: D&D Combat Tracker" />
+				<div class="right">
+					<div class="mr-2">
+						<div class="title">
+							<i class="fas fa-swords" /> Combat Tracker
+						</div>
+						<i class="">Harmless Key</i>						
+					</div>
+					<router-link to="/demo" class="btn btn-sm bg-green my-2">Try demo</router-link>
+				</div>
+			</div>
+		</hk-card>
 		<div class="row q-col-gutter-md">		
 			<div class="col-12" :class="{ 'col-md-9': !this.$route.meta || this.$route.meta.side !== false }">
         <router-view />
@@ -44,6 +58,10 @@
 			show_overencumbered() {
 				const pathArray = this.$route.path.split("/");
 				return pathArray[1] === "content" && this.overencumbered;
+			},
+			show_mobile_ad() {
+				const pathArray = this.$route.path.split("/");
+				return pathArray[1] !== "content" && this.width <= 978;
 			}
 		},
 		methods: {
@@ -53,3 +71,34 @@
 		}
 	}
 </script>
+
+<style lang="scss" scoped>
+	.ad {
+		background-color: $neutral-10;
+
+		.card-body {
+			padding: 10px 15px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			// flex-wrap: wrap;
+	
+			img {
+				margin: 0 15px 0 -5px;
+				width: 80px;
+			}
+			.right {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				flex-wrap: wrap;
+				width: 100%;
+	
+				.title {
+					font-weight: bold;
+					font-size: 20px;
+				}
+			}
+		}
+	}
+</style>

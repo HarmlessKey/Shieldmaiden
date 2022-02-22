fe1d57f5254373b14aeea34293e7e22ba0dedd35
Unknown <keyroos@gmail.com>
2022-02-22 17:52:17 +0100

    Test with most basic service worker that has a blacklist

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,39 +1,40 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
+import { register } from "register-service-worker";
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+      // console.log(
+      //   "[registerServiceWorker] App is being served from cache by a service worker.\n" +
+      //   "For more details, visit https://goo.gl/AFskqB"
+      // )
     },
-    registered (registration) {
-      console.log('Service worker has been registered.', registration)
+    registered () {
+      // console.log("[registerServiceWorker] Service worker has been registered.")
     },
     cached () {
-      console.log('Content has been cached for offline use.')
+      // console.log("[registerServiceWorker] Content has been cached for offline use.")
     },
     updatefound () {
-      console.log('New content is downloading.')
+      // console.log("[registerServiceWorker] New content is downloading.")
     },
-    updated (registration) {
-      console.log('New content is available; please refresh.', registration, caches);
+    updated () {
+      // console.log("[registerServiceWorker] New content is available; please refresh.");
 
       // Cleares old cache so new content is downloaded
       caches.keys().then(function(names) {
         for (let name of names) {
+          console.log("[registerServiceWorker] Cleared cache:", name)
           caches.delete(name);
         }
       });
     },
     offline () {
-      // console.log('No internet connection found. App is running in offline mode.')
+      // console.log("[registerServiceWorker] No internet connection found. App is running in offline mode.")
     },
     error (error) {
-      console.error('Error during service worker registration:', error)
+      console.error("[registerServiceWorker] Error during service worker registration:", error)
     }
   })
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-workbox.core.setCacheNameDetails({prefix: "harmlesskey_app"});
+workbox.core.setCacheNameDetails({prefix: "harmlesskey"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -6,17 +6,35 @@ self.addEventListener('message', (event) => {
   }
 });
 
+workbox.routing.registerNavigationRoute("/index.html", {
+  // blacklist: [
+  //   new RegExp('/compendium'),
+  //   new RegExp('/feedback'),
+  //   new RegExp('/patreon'),
+  //   new RegExp('/user'),
+  //   new RegExp('/about-us'),
+  //   new RegExp('/updates'),
+  //   new RegExp('/documentation'),
+  //   new RegExp('/privacy-policy'),
+  // ],
+  blacklist: [
+    /^\/compendium/,
+    /^\/admin/,
+    /^\/feedback/,
+    /^\/patreon/,
+    /^\/user/,
+    /^\/about-us/,
+    /^\/documentation/,
+    /^\/privacy-policy/,
+  ]
+});
+
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 
-// Blacklist certain URLs for SEO purposes
-// This will prevent the prerendered pages from flashing the home screen first.
-workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/compendium/,/^\/admin/],
-});

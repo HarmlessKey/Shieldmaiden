d8c9fccfd26eea659f9c0776bd3bfd8f763a5d24
Unknown <keyroos@gmail.com>
2022-04-11 17:22:46 +0200

    reinstall animate.css

--- src/boot/plugins.js
+++ src/boot/plugins.js
@@ -3,6 +3,7 @@ import VueFire from 'vuefire';
 import VueCookies from 'vue-cookies'
 import VueAnalytics from 'vue-analytics';
 import numeral from 'vue-numeral-filter';
+import 'animate.css';
 
 export default async ({ router, Vue }) => {
 	Vue.config.productionTip = false;

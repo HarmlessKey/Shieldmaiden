cfb1a8af8a0bd34d356712e52e83e7d2b935bfa7
Harm Manders <harmmanders@gmail.com>
2022-04-26 13:29:49 +0200

    updated shas and action env file

--- src-ssr/index.js
+++ src-ssr/index.js
@@ -23,7 +23,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'", '*.harmlesskey.com'],
-      scriptSrc:  ["'self'", '*.harmlesskey.com', 'cdnjs.cloudflare.com', '*.google-analytics.com', '*.firebaseio.com'],
+      scriptSrc:  ["'self'", '*.harmlesskey.com', 'cdnjs.cloudflare.com', '*.google-analytics.com', '*.firebaseio.com', 'sha512-B8ruywf8mKA4fm0K8SrrNhO5DTokuipFEo7rEnI0RVTt9p29MJKfg8TfOVwMAFZxgI+zOF4iwOA/ByvyJ/sslw==', 'sha512-jrMOSezfLmrgTWzx0L5f490NCRT8V7Tx9EeGvgeyjq/TZU5O+IMmUUPDO/WuKb6F8yCp1cBq8gZeg/Gm40rOfg=='],
       styleSrc:   ["'self'", '*.harmlesskey.com', 'pro.fontawesome.com', 'fonts.googleapis.com'],
       fontSrc:    ["'self'", '*.harmlesskey.com', 'pro.fontawesome.com', 'fonts.gstatic.com'],
       imgSrc:     ["'self'", '*', 'data:'],

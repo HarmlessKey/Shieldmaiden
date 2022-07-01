b1f7cfb04afd323a303df1f227bf73e938e05b4d
Unknown <keyroos@gmail.com>
2022-07-01 13:04:52 +0200

    Login flow fixes

--- src/firebase.js
+++ src/firebase.js
@@ -18,26 +18,4 @@ if(!firebase.apps.length) {
 const auth = firebase.auth();
 const db = firebase.database();
 
-/**
- * Async function providing the application time to
- * wait for firebase to initialize and determine if a
- * user is authenticated or not with only a single observable.
- * https://firebase.google.com/docs/reference/js/firebase.auth.Auth#onauthstatechanged
- * @param {Object} store - Vuex store
- * @returns {Promise} - A promise that return firebase.Unsubscribe
- */
-const ensureAuthIsInitialized = async (store) => {
-    if (store.state.user.uid) return true
-    // Create the observer only once on init
-    return new Promise((resolve, reject) => {
-        // Use a promise to make sure that the router will eventually show the route after the auth is initialized.
-        const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-            resolve()
-            unsubscribe()
-        }, () => {
-            reject(new Error('Looks like there is a problem with the firebase service. Please try again later'))
-        })
-    });
-}
-
-export { firebase, auth, db, ensureAuthIsInitialized };
+export { firebase, auth, db };

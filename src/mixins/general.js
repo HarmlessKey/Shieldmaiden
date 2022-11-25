04f32e530849044090c93aa60db8665091b6be0a
Harm Manders <harmmanders@gmail.com>
2022-11-25 15:06:24 +0100

    date function in generalFunctions

--- src/mixins/general.js
+++ src/mixins/general.js
@@ -1,52 +1,36 @@
-import numeral from 'numeral';
+import numeral from "numeral";
+import { makeDate } from "src/utils/generalFunctions";
 
 export const general = {
 	methods: {
 		calcMod(val) {
-			if(val) {
-				return Math.floor((val - 10) / 2)
-			}
-			else {
+			if (val) {
+				return Math.floor((val - 10) / 2);
+			} else {
 				return 0;
-			}	
+			}
 		},
 		hitDiceStr(npc) {
-			let mod = this.calcMod(npc['constitution']);
-			let nHD = parseInt(npc.hit_dice.split('d')[0]);
+			let mod = this.calcMod(npc["constitution"]);
+			let nHD = parseInt(npc.hit_dice.split("d")[0]);
 			let hdMod = mod * nHD;
 
-			let sign = (hdMod > 0) ? '+' : '';
-			let mod_str = hdMod ? hdMod : '';
-			
+			let sign = hdMod > 0 ? "+" : "";
+			let mod_str = hdMod ? hdMod : "";
+
 			return "" + npc.hit_dice + sign + mod_str;
 		},
 		percentage(current, max) {
-			return Math.floor(current / max * 100);
+			return Math.floor((current / max) * 100);
 		},
-		makeDate(input, showTime = false, short = false) {
-			let monthNames = [
-				"January", "February", "March",
-				"April", "May", "June", "July",
-				"August", "September", "October",
-				"November", "December"
-			];
-
-			let d = new Date(input)
-			let hours = (d.getHours() < 10) ? '0'+d.getHours() : d.getHours();
-			let minutes = (d.getMinutes() < 10) ? '0'+d.getMinutes() : d.getMinutes();
-
-			let time = hours + ":" + minutes;
-			let date = (short) ?
-			d.getDate() + "-" + parseInt(d.getMonth() + 1) + "-" + d.getFullYear() :
-			monthNames[d.getMonth()] + " " + numeral(d.getDate()).format('0o') + " " + d.getFullYear();
-			
-			if(showTime) { return date + " at " + time; }
-			return date
+		makeDate(...args) {
+			return makeDate(...args);
 		},
 		toKebabCase(input) {
-			return input.replace(/(a-z)(A-Z)/g, "$1-$2")
-				.replace(/\s+/g, '-')
+			return input
+				.replace(/(a-z)(A-Z)/g, "$1-$2")
+				.replace(/\s+/g, "-")
 				.toLowerCase();
-		}
-	}
-}
+		},
+	},
+};

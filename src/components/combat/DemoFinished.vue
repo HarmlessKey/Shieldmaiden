cb420689e7b311a54c90e9ef8706df3db793887e
Unknown <keyroos@gmail.com>
2022-12-29 12:20:48 +0100

    Clarify you're logged in when using the demo

--- src/components/combat/DemoFinished.vue
+++ src/components/combat/DemoFinished.vue
@@ -5,7 +5,8 @@
 			<hk-card header="Thanks for trying our demo">
 				<div class="card-body">
 					<h3>We hope you were impressed by the possibilities</h3>
-					<p>This is exactly how you can run your custom created encounters in our app. With your own players, and monsters from the SRD or created by you.</p>
+					<p v-if="!user">This is exactly how you can run your custom created encounters in our app. With your own players, and monsters from the SRD or created by you.</p>
+					<p v-else>Since you have an account, you can start your own campaign and run your encounters from there.</p>
 					<a @click="reload" class="btn bg-neutral-5">
 						<i aria-hidden="true" class="far fa-sync-alt"></i> Reset encounter
 						<q-tooltip anchor="top middle" self="center middle">
@@ -15,7 +16,8 @@
 				</div>
 		
 				<template slot="footer">
-					<router-link class="btn btn-square btn-block btn-lg bg-green" to="/sign-up">Create Account Now</router-link>
+					<router-link v-if="!user" class="btn btn-square btn-block btn-lg bg-green" to="/sign-up">Create Account Now</router-link>
+					<router-link v-else class="btn btn-square btn-block btn-lg bg-green" to="/content">Create custom content</router-link>
 				</template>
 			</hk-card>
 		</div>
@@ -23,12 +25,17 @@
 </template>
 
 <script>
+	import { mapGetters } from "vuex";
+
 	export default {
-		name: 'DemoFinished',
+		name: "DemoFinished",
 		data() {
 			return {
 				
 			}
+		},
+		computed: {
+			...mapGetters(["user"])
 		},
 		methods: {
 			reload() {

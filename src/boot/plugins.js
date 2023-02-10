73bc5bb6a884e29c378a5a8daaf196f9570a610d
Unknown <keyroos@gmail.com>
2023-02-10 13:31:53 +0100

    Add new Analytics side by side with old

--- src/boot/plugins.js
+++ src/boot/plugins.js
@@ -1,8 +1,9 @@
-import VueFire from 'vuefire';
-import VueCookies from 'vue-cookies'
-import VueAnalytics from 'vue-analytics';
-import numeral from 'vue-numeral-filter';
-import 'animate.css';
+import VueFire from "vuefire";
+import VueCookies from "vue-cookies";
+import VueAnalytics from "vue-analytics";
+import VueGtag from "vue-gtag";
+import numeral from "vue-numeral-filter";
+import "animate.css";
 import Croppa from "vue-croppa";
 
 export default async ({ router, Vue }) => {
@@ -10,13 +11,20 @@ export default async ({ router, Vue }) => {
 
 	Vue.use(VueFire);
 	Vue.use(VueCookies);
-	Vue.use(numeral, { locale: 'en' });
+	Vue.use(numeral, { locale: "en" });
 	Vue.use(Croppa);
 
-	require('../functions.js');
+	require("../functions.js");
 
 	Vue.use(VueAnalytics, {
-		id: 'UA-134177767-1',
-		router
+		id: "UA-134177767-1",
+		router,
 	});
+	Vue.use(
+		VueGtag,
+		{
+			config: { id: "G-KDSNCEK6X7" },
+		},
+		router
+	);
 };

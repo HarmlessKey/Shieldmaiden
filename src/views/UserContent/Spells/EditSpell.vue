34793dfece1f744a2782606fcfafc2c6a35b216d
Unknown <keyroos@gmail.com>
2023-03-03 17:14:59 +0100

    BasicInfo form works again

--- src/views/UserContent/Spells/EditSpell.vue
+++ src/views/UserContent/Spells/EditSpell.vue
@@ -1,11 +1,57 @@
 <template>
-	<div class="form">
-		<BasicInfo v-model="editableSpell" />
-		<SpellActions v-model="editableSpell" />
+	<div class="content__edit" v-if="!loading">
+		<ValidationObserver v-slot="{ handleSubmit, valid }">
+			<q-form @submit="handleSubmit(saveSpell)" greedy>
+				<div>
+					<div class="top">
+						<q-icon v-if="!valid" name="error" color="red" size="sm" class="mr-2">
+							<q-tooltip anchor="top middle" self="center middle">
+								There are validation errors
+							</q-tooltip>
+						</q-icon>
+					</div>
+
+					<div class="form">
+						<BasicInfo v-model="spell" />
+						<SpellActions v-model="spell" />
+					</div>
+
+					<!-- HANDLING -->
+					<div class="save">
+						<div class="buttons">
+							<q-icon v-if="!valid" name="error" color="red" size="md" class="mr-2">
+								<q-tooltip anchor="top middle" self="center middle">
+									There are validation errors
+								</q-tooltip>
+							</q-icon>
+							<router-link to="/contribute/spells" class="btn bg-neutral-5 mr-2"
+								>Cancel</router-link
+							>
+							<q-btn label="Save" type="submit" color="primary" no-caps />
+						</div>
+						<div class="d-flex justify-content-start unsaved_changes">
+							<template v-if="unsaved_changes">
+								<div class="orange truncate mr-2 d-none d-md-block">
+									<i aria-hidden="true" class="fas fa-exclamation-triangle"></i> Unsaved changes
+								</div>
+								<a class="btn btn-sm bg-neutral-5" @click="revertChanges()">
+									<i aria-hidden="true" class="fas fa-undo" />
+									Revert
+								</a>
+							</template>
+						</div>
+					</div>
+				</div>
+			</q-form>
+		</ValidationObserver>
 	</div>
+	<hk-card v-else>
+		<hk-loader />
+	</hk-card>
 </template>
 
 <script>
+import { db } from "src/firebase";
 import BasicInfo from "src/components/spells/BasicInfo";
 import SpellActions from "src/components/spells/actions";
 
@@ -15,11 +61,131 @@ export default {
 		BasicInfo,
 		SpellActions,
 	},
-	props: { spell: Object },
+	props: {
+		id: {
+			type: String,
+			required: true,
+		},
+	},
 	data() {
 		return {
-			editableSpell: this.spell,
+			loading: true,
+			spell_copy: undefined,
+			spell: {},
+			unsaved_changes: false,
 		};
+	},
+	async mounted() {
+		const spell_ref = db.ref(`new_spells/${this.id}`);
+		await spell_ref.once("value", (snapshot) => {
+			this.spell = snapshot.val() || {};
+		});
+		this.loading = false;
+	},
+	watch: {
+		spell: {
+			deep: true,
+			handler(newVal) {
+				if (JSON.stringify(newVal) !== this.spell_copy) {
+					this.unsaved_changes = true;
+				} else {
+					this.unsaved_changes = false;
+				}
+				this.$emit("set-unsaved", this.unsaved_changes);
+			},
+		},
+	},
+	methods: {
+		setSpell(spell) {
+			this.spell = spell;
+		},
+		revertChanges() {
+			this.spell = this.spell_copy;
+		},
+		saveSpell() {
+			console.log("Store spell called");
+			delete this.spell[".value"];
+			delete this.spell[".key"];
+
+			this.spell.changed = true;
+			this.spell.checked = false;
+
+			if (this.spell.cast_time_nr) {
+				parseInt(this.spell.cast_time_nr);
+			}
+			if (this.spell.duration_n) {
+				parseInt(this.spell.duration_n);
+			}
+
+			console.log(this.spell);
+
+			db.ref(`new_spells/${this.id}`).set(this.spell);
+			this.$snotify.success("Spell Saved.", "Critical hit!", {
+				position: "rightTop",
+			});
+			this.spell_copy = JSON.stringify(this.spell);
+		},
+	},
+
+	// This is now handled in contribute/spells/edit
+	// Eventually this will be needed here too
+	beforeRouteLeave(to, from, next) {
+		console.log("before route leave");
+		if (this.unsaved_changes) {
+			this.$snotify.error(
+				"There are unsaved changes in the form.\n Would you like to continue?",
+				"Unsaved Changes",
+				{
+					buttons: [
+						{
+							text: "Leave",
+							action: (toast) => {
+								next();
+								this.$snotify.remove(toast.id);
+							},
+							bold: false,
+						},
+						{
+							text: "Stay",
+							action: (toast) => {
+								next(false);
+								this.$snotify.remove(toast.id);
+							},
+							bold: true,
+						},
+					],
+				}
+			);
+		} else {
+			next();
+		}
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+.content__edit {
+	.top {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		margin-bottom: 10px;
+
+		.name {
+			user-select: none;
+			justify-content: flex-end;
+			line-height: 35px;
+
+			.img {
+				width: 31px;
+				height: 31px;
+				background-position: center top;
+				background-size: cover;
+				border: solid 1px $neutral-4;
+				margin: 2px 0 2px 5px;
+				border-radius: 50%;
+			}
+		}
+	}
+}
+</style>
--- src/views/UserContent/Spells/Spells.vue
+++ src/views/UserContent/Spells/Spells.vue
@@ -1,18 +1,11 @@
 <template>
 	<div v-if="tier">
 		<hk-card>
-			<ContentHeader type="npcs">
-				<a slot="actions-left" class="btn btn-sm bg-neutral-5" @click="exportAll()">
-					Export NPCs
-				</a>
-				<a slot="actions-right" class="btn btn-sm bg-neutral-5 mx-2" @click="import_dialog = true">
-					Import NPCs
-				</a>
-			</ContentHeader>
+			<ContentHeader type="spells" />
 
-			<div class="card-body" v-if="!loading_npcs">
-				<p class="neutral-2">These are your custom Non-Player Characters and monsters.</p>
-				<template v-if="npcs.length">
+			<div class="card-body" v-if="!loading_spells">
+				<p class="neutral-2">These are your custom spells.</p>
+				<template v-if="spells.length">
 					<q-input
 						:dark="$store.getters.theme !== 'light'"
 						v-model="search"
@@ -21,34 +14,26 @@
 						square
 						debounce="300"
 						clearable
-						placeholder="Search NPCs"
+						placeholder="Search spells"
 					>
 						<q-icon slot="prepend" name="search" />
 					</q-input>
 
 					<q-table
-						:data="npcs"
-						:visible-columns="visibleColumns"
+						:data="spells"
 						:columns="columns"
 						row-key="key"
 						card-class="bg-none"
 						flat
 						:dark="$store.getters.theme !== 'light'"
-						:loading="loading_npcs"
+						:loading="loading_spells"
 						separator="none"
 						:pagination="{ rowsPerPage: 15 }"
 						:filter="search"
 						wrap-cells
 					>
 						<template v-slot:body-cell="props">
-							<q-td
-								v-if="props.col.name === 'avatar'"
-								class="avatar"
-								:style="avatar(props.row) ? `background-image: url('${avatar(props.row)}')` : ''"
-							>
-								<i aria-hidden="true" v-if="!avatar(props.row)" class="hki-monster" />
-							</q-td>
-							<q-td v-else-if="props.col.name !== 'actions'">
+							<q-td v-if="props.col.name !== 'actions'">
 								<div class="truncate-cell">
 									<div class="truncate">
 										<router-link
@@ -66,89 +51,57 @@
 							<q-td v-else class="text-right d-flex justify-content-between">
 								<router-link class="btn btn-sm bg-neutral-5" :to="`${$route.path}/${props.key}`">
 									<i aria-hidden="true" class="fas fa-pencil" />
-									<q-tooltip anchor="top middle" self="center middle"> Edit </q-tooltip>
+									<q-tooltip anchor="top middle" self="center middle">Edit</q-tooltip>
 								</router-link>
-								<a class="btn btn-sm bg-neutral-5 mx-2" @click="exportNPC(props.key)">
-									<i aria-hidden="true" class="fas fa-arrow-alt-down" />
-									<q-tooltip anchor="top middle" self="center middle"> Download </q-tooltip>
-								</a>
 								<a
 									class="btn btn-sm bg-neutral-5"
 									@click="confirmDelete($event, props.key, props.row)"
 								>
 									<i aria-hidden="true" class="fas fa-trash-alt" />
-									<q-tooltip anchor="top middle" self="center middle"> Delete </q-tooltip>
+									<q-tooltip anchor="top middle" self="center middle">Delete</q-tooltip>
 								</a>
 							</q-td>
 						</template>
 						<div slot="no-data" />
-						<hk-loader slot="loading" name="NPCs" />
+						<hk-loader slot="loading" name="spells" />
 					</q-table>
 				</template>
 
 				<router-link
-					v-if="!npcs.length && !overencumbered"
+					v-if="!spells.length && !overencumbered"
 					class="btn btn-lg bg-neutral-5"
-					to="/content/npcs/add-npc"
+					to="/content/spells/add-spell"
 				>
-					<i aria-hidden="true" class="fas fa-plus green mr-1" /> Create your first NPC
+					<i aria-hidden="true" class="fas fa-plus green mr-1" /> Create your first Spell
 				</router-link>
 				<router-link
 					v-else-if="tier.name === 'Free'"
 					class="btn bg-neutral-8 btn-block"
 					to="/patreon"
 				>
-					Get more NPC slots
+					Get more spell slots
 				</router-link>
-				<q-resize-observer @resize="setSize" />
 			</div>
-			<hk-loader v-else name="NPCs" />
+			<hk-loader v-else name="spells" />
 		</hk-card>
-
-		<!-- Bulk import dialog -->
-		<q-dialog v-model="import_dialog">
-			<hk-card :minWidth="400">
-				<div slot="header" class="card-header">
-					<span>Import NPC from JSON</span>
-					<q-btn padding="sm" size="sm" no-caps icon="fas fa-times" v-close-popup />
-				</div>
-				<div class="card-body">
-					<ImportNPC v-model="import_dialog" />
-				</div>
-			</hk-card>
-		</q-dialog>
 	</div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import { monsterMixin } from "src/mixins/monster";
-import ImportNPC from "src/components/ImportNPC.vue";
 import ContentHeader from "src/components/userContent/ContentHeader";
-import { downloadJSON } from "src/utils/generalFunctions";
 
 export default {
-	name: "Npcs",
-	mixins: [monsterMixin],
+	name: "Spells",
 	components: {
-		ImportNPC,
 		ContentHeader,
 	},
 	data() {
 		return {
 			userId: this.$store.getters.user.uid,
-			import_dialog: false,
-			loading_npcs: true,
+			loading_spells: true,
 			search: "",
-			card_width: 0,
-			overwrite: undefined,
 			columns: [
-				{
-					name: "avatar",
-					label: "",
-					field: "avatar",
-					align: "left",
-				},
 				{
 					name: "name",
 					label: "Name",
@@ -156,21 +109,6 @@ export default {
 					sortable: true,
 					align: "left",
 					format: (val) => val.capitalizeEach(),
-				},
-				{
-					name: "type",
-					label: "Type",
-					field: "type",
-					align: "left",
-					sortable: true,
-				},
-				{
-					name: "challenge_rating",
-					label: "CR",
-					field: "challenge_rating",
-					align: "left",
-					sortable: true,
-					format: (val) => this.cr(val),
 				},
 				{
 					name: "actions",
@@ -182,42 +120,28 @@ export default {
 	},
 	computed: {
 		...mapGetters(["tier", "overencumbered"]),
-		...mapGetters("npcs", ["npcs"]),
-		...mapGetters("players", ["players"]),
-		...mapGetters("campaigns", ["campaigns"]),
-		visibleColumns() {
-			return this.card_width > 600
-				? ["avatar", "name", "type", "challenge_rating", "actions"]
-				: this.card_width > 450
-				? ["avatar", "name", "type", "actions"]
-				: ["avatar", "name", "actions"];
-		},
+		...mapGetters("spells", ["spells"]),
 	},
 	async mounted() {
-		await this.get_npcs();
-		this.loading_npcs = false;
+		await this.get_spells();
+		this.loading_spells = false;
 	},
 	methods: {
 		...mapActions(["setSlide"]),
-		...mapActions("npcs", ["get_npcs", "delete_npc", "get_npc", "get_full_npcs", "add_npc"]),
-		cr(val) {
-			return val == 0.125 ? "1/8" : val == 0.25 ? "1/4" : val == 0.5 ? "1/2" : val;
-		},
-		avatar(npc) {
-			return npc.storage_avatar || npc.avatar;
-		},
-		confirmDelete(e, key, npc) {
+		...mapActions("spells", ["get_spells", "delete_spell"]),
+
+		confirmDelete(e, key, spell) {
 			//Instantly delete when shift is held
 			if (e.shiftKey) {
-				this.deleteNpc(key);
+				this.deleteSpell(key);
 			} else {
-				this.$snotify.error("Are you sure you want to delete " + npc.name + "?", "Delete NPC", {
+				this.$snotify.error("Are you sure you want to delete " + spell.name + "?", "Delete spell", {
 					timeout: false,
 					buttons: [
 						{
 							text: "Yes",
 							action: (toast) => {
-								this.deleteNpc(key);
+								this.deleteSpell(key);
 								this.$snotify.remove(toast.id);
 							},
 							bold: false,
@@ -233,26 +157,8 @@ export default {
 				});
 			}
 		},
-		deleteNpc(key) {
-			this.delete_npc(key);
-		},
-
-		setSize(e) {
-			this.card_width = e.width;
-		},
-		async exportAll() {
-			const all_npcs = await this.get_full_npcs();
-			for (const key in all_npcs) {
-				all_npcs[key].harmless_key = key;
-			}
-
-			const json_export = Object.values(all_npcs);
-			downloadJSON(json_export);
-		},
-		async exportNPC(id) {
-			const npc = await this.get_npc({ uid: this.userId, id });
-			npc.harmless_key = id;
-			downloadJSON(npc);
+		deleteSpell(key) {
+			this.delete_spell(key);
 		},
 	},
 };

c5387e78f0316527d5cfbb902c1f1c36566c67ac
Harm Manders <harmmanders@gmail.com>
2023-03-24 13:19:14 +0100

    Show ritual

--- src/components/compendium/Spell.vue
+++ src/components/compendium/Spell.vue
@@ -2,40 +2,42 @@
 	<div v-if="!loading">
 		<h2 class="mb-1">{{ spell.name }}</h2>
 		<i aria-hidden="true" class="mb-3 d-block">
-			<template v-if="spell.level > 0">
-				{{ spell.level | numeral("oO") }} level
-			</template>
+			<template v-if="spell.level > 0"> {{ spell.level | numeral("oO") }} level </template>
 			<template v-else>Cantrip</template>
 			<span v-if="spell.school"> {{ spell.school.name }}</span>
 		</i>
 		<p>
-			<strong>Casting time:</strong> {{ spell.casting_time }}<br/>
-			<strong>Range:</strong> {{ spell.range }}<br/>
-			<strong>Components:</strong> 
+			<strong>Casting time:</strong> {{ spell.casting_time }}<br />
+			<strong>Range:</strong> {{ spell.range }}<br />
+			<strong>Components:</strong>
 			<template v-for="(component, index) in spell.components">
-				{{ component }}<template v-if="Object.keys(spell.components).length > index + 1">, </template>
+				{{ component
+				}}<template v-if="Object.keys(spell.components).length > index + 1">, </template>
 			</template>
 			<template v-if="spell.material"> ({{ parse_spell_str(spell.material) }})</template>
-			<br/>
+			<br />
+			<template v-if="spell.ritual === 'yes'"> <b>Ritual:</b> {{ spell.ritual }}<br /> </template>
+
 			<strong>Duration:</strong>
-				<template v-if="spell.concentration == 'yes'"> Concentration, </template>
-				{{ spell.duration }}<br/>
+			<template v-if="spell.concentration == 'yes'"> Concentration, </template>
+			{{ spell.duration }}<br />
 			<template v-if="spell.classes">
-				<strong>Classes:</strong> 
+				<strong>Classes:</strong>
 				<template v-for="(_class, index) in spell.classes">
-					{{ _class.name }}<template v-if="Object.keys(spell.classes).length > index + 1">, </template>
+					{{ _class.name
+					}}<template v-if="Object.keys(spell.classes).length > index + 1">, </template>
 				</template>
-				<br/>
+				<br />
 			</template>
 		</p>
-		<hk-dice-text 
-			v-for="(desc, index) in spell.desc" 
-			:input_text="parse_spell_str(desc)" 
+		<hk-dice-text
+			v-for="(desc, index) in spell.desc"
+			:input_text="parse_spell_str(desc)"
 			:key="index"
 		/>
 
 		<p v-if="spell.higher_level">
-			At higher levels. 
+			At higher levels.
 			<template v-for="higher in spell.higher_level">
 				{{ parse_spell_str(higher) }}
 			</template>
@@ -45,72 +47,72 @@
 </template>
 
 <script>
-	import { mapActions } from "vuex";
+import { mapActions } from "vuex";
 
-	export default {
-		name: "Spell",
-		props: {
-			// If the spell is fetched in a parent component you can send the full spell object in de data prop
-			data: {
-				type: Object
-			},
-			// If the id prop is passed, the spell is fetched in the Spell component
-			id: {
-				type: String
-			}
+export default {
+	name: "Spell",
+	props: {
+		// If the spell is fetched in a parent component you can send the full spell object in de data prop
+		data: {
+			type: Object,
 		},
-		data() {
-			return {
-				spell: {},
-				loading: true
-			}
+		// If the id prop is passed, the spell is fetched in the Spell component
+		id: {
+			type: String,
 		},
-		async beforeMount() {
-			if(this.data) {
-				this.spell = this.data;		
-				this.loading = false;
-			} else {
-				this.spell = await this.fetch_api_spell(this.id);
-				this.loading = false;
-			}			
-		},
-		methods: {
-			...mapActions("api_spells", ["fetch_api_spell"]),
-			parse_spell_str(text) {
-				// map to replace weird character with real character 
-				let rules = [
-					{
-						regex: /â€™/g,
-						// eslint-disable-next-line
-						replacement: '\'',
-					},
-					{
-						regex: /â€”/g,
-						// eslint-disable-next-line
-						replacement: '\-\-',
-					},
-					{
-						regex: /â€�/g,
-						// eslint-disable-next-line
-						replacement: '\"'
-					},
-					{
-						regex: /â€œ/g,
-						// eslint-disable-next-line
-						replacement: '\"'
-					},
-					{
-						regex: /â€“/g,
-						// eslint-disable-next-line
-						replacement: '\-\-'
-					},
-				];
-				rules.forEach(function(rule) {
-					text = text.replace(rule.regex, rule.replacement);
-				});
-
-				return text.trim();
-			},
+	},
+	data() {
+		return {
+			spell: {},
+			loading: true,
+		};
+	},
+	async beforeMount() {
+		if (this.data) {
+			this.spell = this.data;
+			this.loading = false;
+		} else {
+			this.spell = await this.fetch_api_spell(this.id);
+			this.loading = false;
 		}
-	};
+	},
+	methods: {
+		...mapActions("api_spells", ["fetch_api_spell"]),
+		parse_spell_str(text) {
+			// map to replace weird character with real character
+			let rules = [
+				{
+					regex: /â€™/g,
+					// eslint-disable-next-line
+					replacement: "'",
+				},
+				{
+					regex: /â€”/g,
+					// eslint-disable-next-line
+					replacement: "\-\-",
+				},
+				{
+					regex: /â€�/g,
+					// eslint-disable-next-line
+					replacement: '"',
+				},
+				{
+					regex: /â€œ/g,
+					// eslint-disable-next-line
+					replacement: '"',
+				},
+				{
+					regex: /â€“/g,
+					// eslint-disable-next-line
+					replacement: "\-\-",
+				},
+			];
+			rules.forEach(function (rule) {
+				text = text.replace(rule.regex, rule.replacement);
+			});
+
+			return text.trim();
+		},
+	},
+};
 </script>

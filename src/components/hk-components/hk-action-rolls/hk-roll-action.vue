f6a3a5fa318eb834151dca907aeec600d72704e1
Unknown <keyroos@gmail.com>
2023-04-07 17:52:58 +0200

    Roll projectiles

--- src/components/hk-components/hk-action-rolls/hk-roll-action.vue
+++ src/components/hk-components/hk-action-rolls/hk-roll-action.vue
@@ -92,24 +92,57 @@ export default {
 			return action[`versatile_${i ? "two" : "one"}`] || `Option ${i + 1}`;
 		},
 		roll(e, action, option) {
-			if (this.$listeners["roll"]) {
-				this.$emit("roll", e, option);
-			} else {
-				const config = {
-					type: this.type,
-					option,
-				};
+			const config = {
+				type: this.type,
+				option,
+			};
 
-				if (this.type === "spell") {
-					config.attack_bonus = this.attackBonus;
-					config.cast_level = this.castLevel;
-					config.caster_level = this.casterLevel;
+			if (this.type === "spell") {
+				config.attack_bonus = this.attackBonus;
+				config.cast_level = this.castLevel;
+				config.caster_level = this.casterLevel;
 
-					action.action_list = action.actions;
-				}
-
-				this.setActionRoll(this.rollAction(e, action, config));
+				action.action_list = action.actions;
 			}
+			const projectiles = this.projectileScaling(config);
+
+			if (this.$listeners["roll"]) {
+				this.$emit("roll", e, projectiles, option);
+			} else {
+				[...Array(projectiles).keys()].forEach(() => {
+					this.setActionRoll(this.rollAction(e, action, config));
+				});
+			}
+		},
+		projectileScaling(config) {
+			let projectiles = this.action.projectiles || 1;
+			const tiers = this.action.projectile_scaling;
+
+			if (tiers && tiers.length) {
+				// SPELL SCALE
+				if (this.action.scaling === "spell_scale") {
+					const scale = tiers[0].level;
+
+					// Calculate the increase based on spell level, on what level the spell is cast and the scale
+					const increase = parseInt(Math.floor((config.cast_level - this.action.level) / scale));
+
+					// If there is an increase,
+					if (increase) {
+						projectiles += increase * tiers[0].projectile_count;
+					}
+				}
+				// CHARACTER LEVEL
+				else if (this.action.scaling === "character_level") {
+					tiers.sort((a, b) => (parseInt(a.level) > parseInt(b.level) ? 1 : -1));
+
+					for (let tier of tiers) {
+						if (parseInt(config.caster_level) >= parseInt(tier.level)) {
+							projectiles = tier.projectile_count;
+						}
+					}
+				}
+			}
+			return projectiles;
 		},
 	},
 };

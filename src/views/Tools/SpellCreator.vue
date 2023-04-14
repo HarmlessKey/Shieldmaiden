931c22a805e8c36e8845afec4dc14bc59d3ac77a
Key <keyroos@gmail.com>
2023-04-14 15:04:49 +0200

    Roll spell in popover

--- src/views/Tools/SpellCreator.vue
+++ src/views/Tools/SpellCreator.vue
@@ -12,22 +12,45 @@
 			</q-btn>
 		</template>
 
-		<section class="mt-5">
+		<section class="mt-4">
 			<p>
 				Our spell creator lets you create custom spells with actions that can be rolled with one
 				click.
 			</p>
 		</section>
 
-		<section class="mt-5">
+		<section class="mt-4">
 			<h2>Scaling</h2>
 			<p>
 				The actions of your spell change depending on caster level, or at what level the spell is
-				casted.
+				casted. Rolls will automatically scale when you cast the spell with a higher lever character, or using a higher spell slot.
 			</p>
+
+			<hk-popover header="Blight (phb 219)">
+				<button class="btn bg-neutral-5 btn-block">Cast Blight</button>
+				<template #content>
+					<div class="caster__levels">
+						<div
+							class="level"
+							:class="{
+								selected: cast_level === i,
+								disabled: i < blight.level,
+							}"
+							v-for="i in 9"
+							:key="i"
+							@click="blight.level <= i ? cast_level = i : null"
+						>
+							{{ i }}
+						</div>
+					</div>
+					<hk-roll-action :action="blight" type="spell" :cast-level="cast_level">
+						<button class="btn btn-block">Cast at level {{ cast_level }}</button>
+					</hk-roll-action>
+				</template>
+			</hk-popover>
 		</section>
 
-		<section class="mt-5">
+		<section class="mt-4">
 			<h2>Spellcasting monsters</h2>
 			<p>
 				You can combine your custom spells with the monsters you've made in Harmless Key to create
@@ -36,7 +59,7 @@
 			</p>
 		</section>
 
-		<section class="mt-5">
+		<section class="mt-4">
 			<h2>Share your creations</h2>
 			<p>
 				Our spell creator also makes it easy to share your custom spells with other dungeon masters
@@ -58,8 +81,65 @@ export default {
 		ToolsPage,
 	},
 	data() {
-		return {};
+		return {
+			blight: {
+				actions: [
+					{
+						name: "Damage roll",
+						rolls: [
+							{
+								damage_type: "necrotic",
+								dice_count: 8,
+								dice_type: 8,
+								save_fail_mod: 0.5,
+								scaling: [
+									{
+										dice_count: 1,
+										dice_type: 8,
+										level: 1
+									}
+								]
+							}
+						],
+						save_ability: "constitution",
+						type: "save"
+					}
+				],
+				level: 4,
+				name: "Blight",
+				scaling: "spell_scale",
+			},
+			cast_level: 4,
+		};
 	},
 	methods: {},
 };
 </script>
+
+<styles lang="scss" scoped>
+.caster__levels {
+	display: flex;
+	justify-content: center;
+	column-gap: 3px;
+	margin-bottom: 10px;
+
+	.level {
+		width: 30px;
+		height: 30px;
+		line-height: 30px;
+		text-align: center;
+		cursor: pointer;
+		background-color: $neutral-8;
+		user-select: none;
+
+		&.selected {
+			background-color: $blue;
+			color: $neutral-1;
+		}
+		&.disabled {
+			opacity: 0.4;
+			cursor: not-allowed;
+		}
+	}
+}
+</styles>

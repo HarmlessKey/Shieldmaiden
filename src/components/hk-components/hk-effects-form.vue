05619bbf2a33087757b1960e9092fc8bbd823065
Unknown <keyroos@gmail.com>
2023-06-02 10:55:34 +0200

    Started effects form

--- src/components/hk-components/hk-effects-form.vue
+++ src/components/hk-components/hk-effects-form.vue
@@ -31,6 +31,69 @@
 				:error-message="errors[0]"
 			/>
 		</ValidationProvider>
+
+		<q-select
+			:dark="$store.getters.theme === 'dark'"
+			filled
+			square
+			emit-value
+			map-options
+			label="Trigger"
+			class="mb-3"
+			clearable
+			v-model="effect.trigger"
+			:options="triggers"
+		>
+			<hk-popover slot="append" header="Triggers">
+				<i class="fas fa-info-circle neutral-2" aria-hidden="true" />
+				<template #content>
+					With triggers effects can be applied when a certain condition is met. This won't work for
+					bonus type effects, but it's designed for effects where a target has to take damage at a
+					certain moment, or can attempt a saving throw for instance.
+				</template>
+			</hk-popover>
+		</q-select>
+
+		<q-select
+			:dark="$store.getters.theme === 'dark'"
+			filled
+			square
+			emit-value
+			map-options
+			label="Duration type"
+			class="mb-3"
+			clearable
+			v-model="effect.duration_type"
+			:options="duration_types"
+		/>
+
+		<ValidationProvider
+			v-if="effect.duration_type === 'time'"
+			rules="between:0,99"
+			name="Duration"
+			v-slot="{ errors, invalid, validated }"
+		>
+			<q-input
+				:dark="$store.getters.theme === 'dark'"
+				filled
+				square
+				label="Duration (in rounds)"
+				v-model="effect.duration"
+				autocomplete="off"
+				type="number"
+				class="mb-2"
+				hint="1 round is 6 seconds"
+				:error="invalid && validated"
+				:error-message="errors[0]"
+				@keyup="$forceUpdate()"
+				@input="(value) => parseToInt(value, effect, 'duration')"
+			>
+				<hk-popover slot="append" header="Duration">
+					<i class="fas fa-info-circle neutral-2" aria-hidden="true" />
+					<template #content>For how long is the effect applied to the target?</template>
+				</hk-popover>
+			</q-input>
+		</ValidationProvider>
 	</div>
 </template>
 
@@ -43,6 +106,50 @@ export default {
 			required: true,
 		},
 	},
+	data() {
+		return {
+			triggers: [
+				{
+					label: "Start of casters turn",
+					value: "start_turn_caster",
+				},
+				{
+					label: "End of casters turn",
+					value: "end_turn_caster",
+				},
+				{
+					label: "End of targets turn",
+					value: "end_turn_target",
+				},
+				{
+					label: "End of targets turn",
+					value: "end_turn_target",
+				},
+				{
+					label: "On damage taken",
+					value: "damage_taken",
+				},
+			],
+			duration_types: [
+				{
+					label: "Time in rounds",
+					value: "time",
+				},
+				{
+					label: "Until cancelled",
+					value: "cancelled",
+				},
+				{
+					label: "Until saved",
+					value: "saved",
+				},
+				{
+					label: "Concentration break",
+					value: "concentration",
+				},
+			],
+		};
+	},
 	computed: {
 		effect: {
 			get() {
@@ -51,6 +158,15 @@ export default {
 			set(newVal) {
 				this.$emit("input", newVal);
 			},
+		},
+	},
+	methods: {
+		parseToInt(value, object, property) {
+			if (value === undefined || value === "") {
+				this.$delete(object, property);
+			} else {
+				this.$set(object, property, parseInt(value));
+			}
 		},
 	},
 };
--- src/mixins/effects.js
+++ src/mixins/effects.js
@@ -2,22 +2,22 @@ export const effects = {
 	data() {
 		return {
 			effect_types: {
-				bonus: { 
+				bonus: {
 					label: "Bonus",
 					value: "bonus",
 					info: "Add a bonus value to a stat. (Can be negative)",
 					form_fields: { values: true },
 				},
-				set: { 
+				set: {
 					label: "Set",
 					value: "set",
-					info: "Set a stat to a set value. This set value allows later bonusses from other effects.",
+					info: "Set a stat to a set value. This set value allows later bonuses from other effects.",
 					form_fields: { values: true },
 				},
-				fixed: { 
+				fixed: {
 					label: "Fixed",
 					value: "fixed",
-					info: "Set a stat to fixed value. This fixed value ignores all other bonusses.",
+					info: "Set a stat to fixed value. This fixed value ignores all other bonuses.",
 					form_fields: { values: true },
 				},
 				defenses: {
@@ -28,16 +28,16 @@ export const effects = {
 				},
 				advantage: {
 					label: "Advantage",
-					value: "advantage"
+					value: "advantage",
 				},
 				disadvantage: {
 					label: "Disadvantage",
-					value: "disadvantage"
-				}, 
+					value: "disadvantage",
+				},
 				special: {
 					label: "Special",
 					value: "special",
-					info: "Special effects that don't fall into the other categories."
+					info: "Special effects that don't fall into the other categories.",
 				},
 				// description: {
 				// 	label: "Description",
@@ -52,7 +52,7 @@ export const effects = {
 					attack: { label: "Attack Rolls", value: "attack" },
 					damage: { label: "Damage Rolls", value: "damage", form_fields: { damage_types: true } },
 					ability: { label: "Ability Checks", value: "ability", form_fields: { abilities: true } },
-					skill: {label: "Skill Checks", value: "skill", form_fields: {skills: true } },
+					skill: { label: "Skill Checks", value: "skill", form_fields: { skills: true } },
 					save: { label: "Saving Throws", value: "save", form_fields: { abilities: true } },
 				},
 				set: {
@@ -62,38 +62,44 @@ export const effects = {
 				fixed: {
 					AC: { label: "Armor Class", value: "AC", form_fields: { minimum: true } },
 					maxHp: { label: "Maximum Hit Points", value: "maxHp" },
-					tempHp: { label: "Temporary Hit Points", value: "tempHp" }
+					tempHp: { label: "Temporary Hit Points", value: "tempHp" },
 				},
 				defenses: {
 					v: { label: "Vulnerable", value: "v" },
 					r: { label: "Resistant", value: "r" },
-					i: { label: "Immune", value: "i" }
+					i: { label: "Immune", value: "i" },
 				},
 				advantage: {
-					attack: { label: "Attack Rolls", value: "attack", form_fields: { attack: true }  },
+					attack: { label: "Attack Rolls", value: "attack", form_fields: { attack: true } },
 					ability: { label: "Ability Checks", value: "ability", form_fields: { abilities: true } },
-					skill: {label: "Skill Checks", value: "skill", form_fields: {skills: true } },
+					skill: { label: "Skill Checks", value: "skill", form_fields: { skills: true } },
 					save: { label: "Saving Throws", value: "save", form_fields: { abilities: true } },
 					death_save: { label: "Death Saving Throws", value: "death_save" },
 				},
 				disadvantage: {
 					attack: { label: "Attack Rolls", value: "attack", form_fields: { attack: true } },
 					ability: { label: "Ability Checks", value: "ability", form_fields: { abilities: true } },
-					skill: {label: "Skill Checks", value: "skill", form_fields: {skills: true } },
+					skill: { label: "Skill Checks", value: "skill", form_fields: { skills: true } },
 					save: { label: "Saving Throws", value: "save", form_fields: { abilities: true } },
-					death_save: { label: "Death Saving Throws", value: "death_save", },
+					death_save: { label: "Death Saving Throws", value: "death_save" },
 				},
 				special: {
 					max_healing: { label: "Maximum Healing", value: "max_healing" },
-					max_damage: { label: "Maximum Damage", value: "max_damage", form_fields: { damage_types: true } },
+					max_damage: {
+						label: "Maximum Damage",
+						value: "max_damage",
+						form_fields: { damage_types: true },
+					},
 					death_ward: { label: "Death Ward", value: "death_ward" },
-					description: { label: "Description", value: "description", form_fields: { description: true } },
+					description: {
+						label: "Description",
+						value: "description",
+						form_fields: { description: true },
+					},
 				},
 				// description: {}
-			}
-		}
+			},
+		};
 	},
-	methods: {
-		
-	}
-}
+	methods: {},
+};

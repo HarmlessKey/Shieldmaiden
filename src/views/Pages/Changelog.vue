e1febbd9bc73da9ebd293323798e184bb20377ec
Unknown <keyroos@gmail.com>
2023-06-23 14:37:43 +0200

    Only show 10 results on changelog page

--- src/views/Pages/Changelog.vue
+++ src/views/Pages/Changelog.vue
@@ -2,9 +2,19 @@
 	<hk-card>
 		<div class="card-header" slot="header">
 			<h1>Changelog</h1>
+			<a
+				class="btn btn-sm bg-neutral-5"
+				href="https://github.com/HarmlessKey/HarmlessKey/releases"
+				target="_blank"
+				rel="noopener"
+			>
+				<i class="fab fa-github mr-1" aria-hidden="true" />
+				Show all
+			</a>
 		</div>
 		<hk-loader name="releases" v-if="loading" />
 		<div v-else class="card-body">
+			<h3>Our 10 latest releases</h3>
 			<q-list :dark="$store.getters.theme === 'dark'" class="accordion">
 				<q-expansion-item
 					v-for="release in releases"
@@ -14,7 +24,14 @@
 					group="release"
 				>
 					<template v-slot:header>
-						<q-item-section>{{ release.name }}</q-item-section>
+						<q-item-section>
+							<q-item-label>
+								<strong>{{ release.name }}</strong>
+							</q-item-label>
+							<q-item-label caption class="neutral-3">{{
+								release.created_at.split("T")[0]
+							}}</q-item-label>
+						</q-item-section>
 						<q-item-section avatar>
 							<q-chip color="primary" square :dark="$store.getters.theme !== 'light'" size="12px">
 								<strong>
@@ -23,12 +40,22 @@
 							</q-chip>
 						</q-item-section>
 					</template>
-					<div class="p-4 bg-neutral-8 accoridion-body">
+					<div class="p-4 bg-neutral-8 accordion-body">
 						<h3>{{ makeDate(new Date(release.created_at)) }}</h3>
 						<div v-html="parsedMarkdown(release.body)"></div>
 					</div>
 				</q-expansion-item>
 			</q-list>
+
+			<a
+				class="btn btn-block bg-neutral-5 mt-3"
+				href="https://github.com/HarmlessKey/HarmlessKey/releases"
+				target="_blank"
+				rel="noopener"
+			>
+				<i class="fab fa-github mr-1" aria-hidden="true" />
+				Show all releases
+			</a>
 		</div>
 	</hk-card>
 </template>
@@ -62,6 +89,8 @@ export default {
 				.request("GET /repos/{owner}/{repo}/releases", {
 					owner: "HarmlessKey",
 					repo: "HarmlessKey",
+					page: 1,
+					per_page: 10,
 				})
 				.catch((e) => {
 					console.log(e);

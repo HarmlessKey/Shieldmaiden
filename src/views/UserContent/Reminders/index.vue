525674abaa689fc06d1bca9caf6d5d9fa7073c91
Harm Manders <harmmanders@gmail.com>
2023-09-29 15:19:26 +0200

    store reminders under a key

--- src/views/UserContent/Reminders/index.vue
+++ src/views/UserContent/Reminders/index.vue
@@ -3,8 +3,8 @@
 		<ContentHeader type="reminders" />
 		<div class="card-body" v-if="!loading_reminders">
 			<p class="neutral-2">
-				Reminders create useful notifications during encounters, 
-				so you don't forget someone was concentrating for instance.
+				Reminders create useful notifications during encounters, so you don't forget someone was
+				concentrating for instance.
 			</p>
 
 			<template v-if="reminders.length">
@@ -22,10 +22,9 @@
 					wrap-cells
 				>
 					<template v-slot:body-cell="props">
-
 						<q-td v-if="props.col.name !== 'actions'">
 							<div class="truncate-cell">
-								<div class="truncate"> 
+								<div class="truncate">
 									<router-link v-if="props.col.name === 'name'" :to="`${$route.path}/${props.key}`">
 										{{ props.value }}
 									</router-link>
@@ -39,15 +38,14 @@
 						<q-td v-else class="text-right d-flex justify-content-between">
 							<router-link class="btn btn-sm bg-neutral-5" :to="`${$route.path}/${props.key}`">
 								<i aria-hidden="true" class="fas fa-pencil"></i>
-								<q-tooltip anchor="top middle" self="center middle">
-									Edit
-								</q-tooltip>
+								<q-tooltip anchor="top middle" self="center middle"> Edit </q-tooltip>
 							</router-link>
-							<a class="btn btn-sm bg-neutral-5 ml-2" @click="confirmDelete($event, props.key, props.row)">
+							<a
+								class="btn btn-sm bg-neutral-5 ml-2"
+								@click="confirmDelete($event, props.key, props.row)"
+							>
 								<i aria-hidden="true" class="fas fa-trash-alt"></i>
-								<q-tooltip anchor="top middle" self="center middle">
-									Delete
-								</q-tooltip>
+								<q-tooltip anchor="top middle" self="center middle"> Delete </q-tooltip>
 							</a>
 						</q-td>
 					</template>
@@ -55,10 +53,18 @@
 				</q-table>
 			</template>
 
-			<router-link v-if="!reminders.length && !overencumbered" class="btn btn-lg bg-neutral-5" :to="`${$route.path}/add-reminder`">
+			<router-link
+				v-if="!reminders.length && !overencumbered"
+				class="btn btn-lg bg-neutral-5"
+				:to="`${$route.path}/add-reminder`"
+			>
 				<i aria-hidden="true" class="fas fa-plus green"></i> Create your first reminder
 			</router-link>
-			<router-link v-else-if="tier.name === 'Free'" class="btn bg-neutral-8 btn-block" to="/patreon">
+			<router-link
+				v-else-if="tier.name === 'Free'"
+				class="btn bg-neutral-8 btn-block"
+				to="/patreon"
+			>
 				Get more reminder slots
 			</router-link>
 		</div>
@@ -67,76 +73,79 @@
 </template>
 
 <script>
-	import { mapActions, mapGetters } from 'vuex';
-	import ContentHeader from "src/components/userContent/ContentHeader";
+import { mapActions, mapGetters } from "vuex";
+import ContentHeader from "src/components/userContent/ContentHeader";
 
-	export default {
-		name: 'Reminders',
-		components: {
-			ContentHeader
-		},
-		data() {
-			return {
-				userId: this.$store.getters.user.uid,
-				loading_reminders: true,
-				search: "",
-				columns: [
+export default {
+	name: "Reminders",
+	components: {
+		ContentHeader,
+	},
+	data() {
+		return {
+			userId: this.$store.getters.user.uid,
+			loading_reminders: true,
+			search: "",
+			columns: [
+				{
+					name: "name",
+					label: "Name",
+					field: "title",
+					sortable: true,
+					align: "left",
+				},
+				{
+					name: "actions",
+					label: "",
+					align: "right",
+				},
+			],
+		};
+	},
+	computed: {
+		...mapGetters(["tier", "overencumbered"]),
+		...mapGetters("reminders", ["reminders"]),
+	},
+	async mounted() {
+		await this.get_reminders();
+		this.loading_reminders = false;
+	},
+	methods: {
+		...mapActions("reminders", ["get_reminders", "delete_reminder"]),
+		confirmDelete(e, key, reminder) {
+			//Instantly delete when shift is held
+			if (e.shiftKey) {
+				this.deleteReminder(key);
+			} else {
+				this.$snotify.error(
+					"Are you sure you want to delete " + reminder + "?",
+					"Delete reminder",
 					{
-						name: "title",
-						label: "Title",
-						field: "title",
-						sortable: true,
-						align: "left",
-					},
-					{
-						name: "actions",
-						label: "",
-						align: "right"
-					}
-				]
-			}
-		},
-		computed: {
-			...mapGetters([
-				'tier',
-				'overencumbered',
-			]),
-			...mapGetters("reminders", ["reminders"])
-		},
-		async mounted() {
-			await this.get_reminders();
-			this.loading_reminders = false;
-		},
-		methods: {
-			...mapActions("reminders", ["get_reminders", "delete_reminder"]),
-			confirmDelete(e, key, reminder) {
-				//Instantly delete when shift is held
-				if(e.shiftKey) {
-					this.deleteReminder(key);
-				} else {
-					this.$snotify.error('Are you sure you want to delete ' + reminder + '?', 'Delete reminder', {
 						timeout: false,
 						buttons: [
 							{
-								text: 'Yes', action: (toast) => { 
-								this.deleteReminder(key)
-								this.$snotify.remove(toast.id); 
-								}, 
-								bold: false
+								text: "Yes",
+								action: (toast) => {
+									this.deleteReminder(key);
+									this.$snotify.remove(toast.id);
+								},
+								bold: false,
 							},
 							{
-								text: 'No', action: (toast) => { 
-									this.$snotify.remove(toast.id); 
-								}, 
-								bold: true
+								text: "No",
+								action: (toast) => {
+									this.$snotify.remove(toast.id);
+								},
+								bold: true,
 							},
-						]
-					});
-				}
-			},
-			deleteReminder(key) {
-				this.delete_reminder(key);
+						],
+					}
+				);
 			}
-		}
-	}
+		},
+		deleteReminder(key) {
+			this.delete_reminder(key);
+		},
+	},
+};
 </script>

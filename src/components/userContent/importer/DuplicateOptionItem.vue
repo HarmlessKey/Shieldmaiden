a7c5213e11b610dc2b5b8fcd89c03f735c276353
Harm Manders <harmmanders@gmail.com>
2023-10-15 15:19:19 +0200

    duplicate option sytling

--- src/components/userContent/importer/DuplicateOptionItem.vue
+++ src/components/userContent/importer/DuplicateOptionItem.vue
@@ -1,13 +1,11 @@
 <template>
-	<div class="mx-1 p-2">
-		<!-- <h3>{{ input.name }}</h3> -->
-		<div v-for="(value, prop) in input" :key="prop" class="d-flex justify-content-between">
-			<strong>
-				{{ prop }}
-			</strong>
-			<span>
-				{{ value }}
-			</span>
+	<div class="mini-stat-block mx-1 p-2">
+		<strong class="name">{{ input.name.toLowerCase().capitalizeEach() }}</strong>
+		<div v-for="stat in stat_properties" :key="stat.prop" class="d-flex justify-content-between">
+			<template v-if="input[stat.prop]">
+				<strong class="mr-1">{{ stat.label }}:</strong>
+				<span>{{ input[stat.prop].toString().capitalize() }}</span>
+			</template>
 		</div>
 	</div>
 </template>
@@ -22,7 +20,29 @@ export default {
 			required: true,
 		},
 	},
+	data() {
+		return {
+			stat_properties: [
+				{ prop: "challenge_rating", label: "Cr" },
+				{ prop: "type", label: "Type" },
+				{ prop: "level", label: "Level" },
+				{ prop: "school", label: "School" },
+			],
+		};
+	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.mini-stat-block {
+	border: 1px black solid;
+	border-radius: $border-radius-small;
+	background-color: $neutral-5;
+}
+
+// .name {
+// 	overflow: hidden;
+// 	white-space: nowrap;
+// 	text-overflow: ellipsis;
+// }
+</style>

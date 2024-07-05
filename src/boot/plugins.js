507967453839edf87776bf8983cf0e5bbcf1641c
Keyzer <keyroos@gmail.com>
2024-07-05 17:12:37 +0200

    Turn of debug for gtag

--- src/boot/plugins.js
+++ src/boot/plugins.js
@@ -27,7 +27,7 @@ export default async ({ router, Vue }) => {
 		compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
 		nonce: "2726c7f26c", // Will add `nonce` to the script tag
 		enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
-		debug: true, // Whether or not display console logs debugs (optional)
+		debug: false, // Whether or not display console logs debugs (optional)
 		loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
 		vueRouter: router, // Pass the router instance to automatically sync with router (optional)
 		ignoredViews: [], // Don't trigger events for specified router names (optional)

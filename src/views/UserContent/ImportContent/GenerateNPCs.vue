df1bd5125bf6c8a03e01d6f7d7c1f57bf9b5b2e6
Keyzer <keyroos@gmail.com>
2024-10-11 15:17:04 +0200

    Show selected image and convert to base64 string

--- src/views/UserContent/ImportContent/GenerateNPCs.vue
+++ src/views/UserContent/ImportContent/GenerateNPCs.vue
@@ -2,17 +2,22 @@
 	<hk-card header="Generate NPCs">
 		<div class="card-body">
 			<q-file
+				v-if="!file"
+				v-model="file"
 				:dark="$store.getters.theme === 'dark'"
-				filled
-				square
-				accept=".jpg, image/*"
-				v-model="image"
 				label="Drag an image here or click to upload"
+				accept=".jpg, image/*"
+				square
+				filled
+				@input="convertToBase64"
 			>
 				<template v-slot:prepend>
-					<q-icon name="attach_file" />
+					<q-icon name="fas fa-image" />
 				</template>
 			</q-file>
+			<template v-else>
+				<img :src="image" class="preview" />
+			</template>
 		</div>
 	</hk-card>
 </template>
@@ -22,8 +27,40 @@ export default {
 	name: "generate-npcs",
 	data() {
 		return {
-			image: null,
+			file: null,
+			base64String: null,
 		};
+	},
+	computed: {
+		image() {
+			return this.file ? URL.createObjectURL(this.file) : null;
+		},
+	},
+	methods: {
+		convertToBase64(file) {
+			if (file) {
+				const reader = new FileReader();
+				reader.onload = (e) => {
+					this.base64String = e.target.result;
+				};
+				reader.readAsDataURL(file);
+			}
+		},
+		cancel() {
+			this.file = null;
+			this.base64String = null;
+		},
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+.preview {
+	width: 100%;
+	object-fit: contain;
+	max-height: 400px;
+	padding: 15px;
+	background-color: $neutral-9;
+	border-radius: $border-radius;
+}
+</style>

41df51dd1e99ba7aa048b838a5d8c4d0e5e517f9
Harm Manders <harmmanders@gmail.com>
2026-03-13 19:04:27 +0100

    Map CR slider to fractional values (0, 1/8, 1/4, 1/2, 1-30)
    Slider now uses 34 positions mapping to actual CR values so the first
    ticks display 0, 1/8, 1/4, 1/2 before hitting 1. Labels and stored
    filter values both reflect the correct CR numbers.

--- src/components/hk-components/hk-filter.vue
+++ src/components/hk-components/hk-filter.vue
@@ -27,7 +27,9 @@
 				v-model="cr"
 				label-always
 				:min="0"
-				:max="30"
+				:max="33"
+				:left-label-value="crLabel(cr.min)"
+				:right-label-value="crLabel(cr.max)"
 				class="px-2"
 				@input="setCR"
 			/>
@@ -75,7 +77,9 @@
 				v-model="cr"
 				label-always
 				:min="0"
-				:max="30"
+				:max="33"
+				:left-label-value="crLabel(cr.min)"
+				:right-label-value="crLabel(cr.max)"
 				class="px-2"
 				@input="setCR"
 			/>
@@ -117,6 +121,14 @@ import { monsterMixin } from "src/mixins/monster.js";
 import { spell_schools } from "src/utils/spellConstants";
 import numeral from "numeral";
 
+const CR_VALUES = [0, 0.125, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+const CR_LABELS = ["0", "1/8", "1/4", "1/2", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"];
+
+function crToPosition(cr) {
+	const idx = CR_VALUES.indexOf(cr);
+	return idx >= 0 ? idx : CR_VALUES.length - 1;
+}
+
 export default {
 	name: "hk-filter",
 	mixins: [monsterMixin],
@@ -135,9 +147,12 @@ export default {
 		},
 	},
 	data() {
+		const existingCR = this.value?.challenge_ratings;
 		return {
 			spell_schools: spell_schools,
-			cr: this.value?.challenge_ratings || { min: 0, max: 30 },
+			cr: existingCR
+				? { min: crToPosition(existingCR.min), max: crToPosition(existingCR.max) }
+				: { min: 0, max: 33 },
 			levels: this.value?.levels || { min: 0, max: 9 },
 		}
 	},
@@ -168,6 +183,9 @@ export default {
 		}
 	},
 	methods: {
+		crLabel(position) {
+			return CR_LABELS[position] ?? String(position);
+		},
 		setLevels(value) {
 			if(!this.filter.levels) {
 				this.$set(this.filter, "levels", {});
@@ -180,8 +198,8 @@ export default {
 			if(!this.filter.challenge_ratings) {
 				this.$set(this.filter, "challenge_ratings", {});
 			}
-			this.$set(this.filter.challenge_ratings, "min", value.min);
-			this.$set(this.filter.challenge_ratings, "max", value.max);
+			this.$set(this.filter.challenge_ratings, "min", CR_VALUES[value.min]);
+			this.$set(this.filter.challenge_ratings, "max", CR_VALUES[value.max]);
 			this.$forceUpdate();
 		}
 	}
